cb58b384b951d246c72563746980f4a7264f83bc
Roger Kratz <roger.kratz@gmail.com>
2011-01-10 08:48:42 +0000

    some notes to myself + cleaning up

--- Src/NHibernate.Envers/Configuration/AuditConfiguration.cs
+++ Src/NHibernate.Envers/Configuration/AuditConfiguration.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using NHibernate.Envers.Entities;
 using NHibernate.Envers.RevisionInfo;
@@ -9,78 +6,46 @@ using NHibernate.Envers.Synchronization;
 
 namespace NHibernate.Envers.Configuration
 {
-    /**
-     * @author Simon Duduica, port of Envers omonyme class by Adam Warski (adam at warski dot org)
-     */
-    public class AuditConfiguration 
-    {
-        public virtual GlobalConfiguration GlobalCfg { get; private set; }
-        public virtual AuditEntitiesConfiguration AuditEntCfg { get; private set; }
-        public virtual AuditSyncManager AuditSyncManager { get; private set; }
-        public virtual EntitiesConfigurations EntCfg { get; private set; }
-        public virtual RevisionInfoQueryCreator RevisionInfoQueryCreator { get; private set; }
-        public virtual RevisionInfoNumberReader RevisionInfoNumberReader { get; private set; }
+	public class AuditConfiguration 
+	{
+		public AuditConfiguration(Cfg.Configuration cfg) 
+		{
+			var properties = cfg.Properties;
+			var revInfoCfg = new RevisionInfoConfiguration(); //rk - when configure other than attr, check here
+			var revInfoCfgResult = revInfoCfg.Configure(cfg); //rk - when configure other than attr, check here
+			AuditEntCfg = new AuditEntitiesConfiguration(properties, revInfoCfgResult.RevisionInfoEntityName);
+			GlobalCfg = new GlobalConfiguration(properties);
+			AuditSyncManager = new AuditSyncManager(revInfoCfgResult.RevisionInfoGenerator);
+			RevisionInfoQueryCreator = revInfoCfgResult.RevisionInfoQueryCreator;
+			RevisionInfoNumberReader = revInfoCfgResult.RevisionInfoNumberReader;
+			EntCfg = new EntitiesConfigurator().Configure(cfg, GlobalCfg, AuditEntCfg, //rk - when configure other than attr, check here (annotationmetadatareader & auditedpropertiesreader)
+					revInfoCfgResult.RevisionInfoXmlMapping, revInfoCfgResult.RevisionInfoRelationMapping);
+		}
 
-        //public AuditEntitiesConfiguration getAuditEntCfg() {
-        //    return auditEntCfg;
-        //}
+		public virtual GlobalConfiguration GlobalCfg { get; private set; }
+		public virtual AuditEntitiesConfiguration AuditEntCfg { get; private set; }
+		public virtual AuditSyncManager AuditSyncManager { get; private set; }
+		public virtual EntitiesConfigurations EntCfg { get; private set; }
+		public virtual RevisionInfoQueryCreator RevisionInfoQueryCreator { get; private set; }
+		public virtual RevisionInfoNumberReader RevisionInfoNumberReader { get; private set; }
 
-        //public AuditSyncManager getSyncManager() {
-        //    return auditSyncManager;
-        //}
+		private static readonly IDictionary<Cfg.Configuration, AuditConfiguration> cfgs
+				= new Dictionary<Cfg.Configuration, AuditConfiguration>();
+				//ORIG: = new WeakHashMap<Cfg.Configuration, AuditConfiguration>(); TODO Simon see if it's ok
 
-        //public GlobalConfiguration getGlobalCfg() {
-        //    return globalCfg;
-        //}
+		[MethodImpl(MethodImplOptions.Synchronized)]
+		public static AuditConfiguration getFor(Cfg.Configuration cfg) 
+		{
+			AuditConfiguration verCfg;
+			if (!cfgs.TryGetValue(cfg, out verCfg))
+			{
+				verCfg = new AuditConfiguration(cfg);
+				cfgs.Add(cfg, verCfg);
+				
+				cfg.BuildMappings();
+			}
 
-        //public EntitiesConfigurations getEntCfg() {
-        //    return entCfg;
-        //}
-
-        //public RevisionInfoQueryCreator getRevisionInfoQueryCreator() {
-        //    return revisionInfoQueryCreator;
-        //}
-
-        //public RevisionInfoNumberReader getRevisionInfoNumberReader() {
-        //    return revisionInfoNumberReader;
-        //}
-
-        //TODO Simon @SuppressWarnings({"unchecked"})
-
-        public AuditConfiguration(NHibernate.Cfg.Configuration cfg) {
-            IDictionary<string,string> properties = cfg.Properties;
-
-            //ReflectionManager reflectionManager = ((AnnotationConfiguration) cfg).getReflectionManager();
-            RevisionInfoConfiguration revInfoCfg = new RevisionInfoConfiguration();
-            RevisionInfoConfigurationResult revInfoCfgResult = revInfoCfg.Configure(cfg);
-            AuditEntCfg = new AuditEntitiesConfiguration(properties, revInfoCfgResult.RevisionInfoEntityName);
-            GlobalCfg = new GlobalConfiguration(properties);
-            AuditSyncManager = new AuditSyncManager(revInfoCfgResult.RevisionInfoGenerator);
-            RevisionInfoQueryCreator = revInfoCfgResult.RevisionInfoQueryCreator;
-            RevisionInfoNumberReader = revInfoCfgResult.RevisionInfoNumberReader;
-            EntCfg = new EntitiesConfigurator().Configure(cfg, GlobalCfg, AuditEntCfg,
-                    revInfoCfgResult.RevisionInfoXmlMapping, revInfoCfgResult.RevisionInfoRelationMapping);
-        }
-
-        //
-
-        private static IDictionary<Cfg.Configuration, AuditConfiguration> cfgs
-                = new Dictionary<Cfg.Configuration, AuditConfiguration>();
-                //ORIG: = new WeakHashMap<Cfg.Configuration, AuditConfiguration>(); TODO Simon see if it's ok
-
-        [MethodImpl(MethodImplOptions.Synchronized)]
-        public static AuditConfiguration getFor(Cfg.Configuration cfg) {
-            AuditConfiguration verCfg = null;
-            if(cfgs.Keys.Contains(cfg))
-                verCfg = cfgs[cfg];
-            else{
-                verCfg = new AuditConfiguration(cfg);
-                cfgs.Add(cfg, verCfg);
-                
-                cfg.BuildMappings();
-            }
-
-            return verCfg;
-        }
-    }
+			return verCfg;
+		}
+	}
 }
