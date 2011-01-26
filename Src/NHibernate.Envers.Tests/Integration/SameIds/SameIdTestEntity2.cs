626c010095a9b8059150bef810e574263ea456fc
Roger Kratz <roger.kratz@gmail.com>
2011-01-26 12:30:37 +0000

    sameidstest

---
+++ Src/NHibernate.Envers.Tests/Integration/SameIds/SameIdTestEntity2.cs
@@ -0,0 +1,22 @@
+namespace NHibernate.Envers.Tests.Integration.SameIds
+{
+	public class SameIdTestEntity2
+	{
+		public virtual int Id { get; set; }
+		[Audited]
+		public virtual string Str1 { get; set; }
+
+		public override bool Equals(object obj)
+		{
+			var other = obj as SameIdTestEntity2;
+			if (other == null)
+				return false;
+			return Id == other.Id && Str1.Equals(other.Str1);
+		}
+
+		public override int GetHashCode()
+		{
+			return Id.GetHashCode() ^ Str1.GetHashCode();
+		}
+	}
+}
---
+++ Src/NHibernate.Envers.Tests/Integration/SameIds/SameIdsTest.cs
@@ -0,0 +1,53 @@
+using NUnit.Framework;
+
+namespace NHibernate.Envers.Tests.Integration.SameIds
+{
+	[TestFixture]
+	public class SameIdsTest : TestBase
+	{
+		protected override void Initialize()
+		{
+			var site1 = new SameIdTestEntity1 {Id = 1, Str1 = "str1"};
+			var site2 = new SameIdTestEntity2 {Id = 1, Str1 = "str1"};
+			using (var tx = Session.BeginTransaction())
+			{
+				Session.Save(site1);
+				Session.Save(site2);
+				tx.Commit();
+			}
+			using (var tx = Session.BeginTransaction())
+			{
+				site1.Str1 = "str2";
+				site2.Str1 = "str2";
+				tx.Commit();
+			}
+		}
+
+		[Test]
+		public void VerifyRevisionCount()
+		{
+			CollectionAssert.AreEquivalent(new[] { 1, 2 }, AuditReader().GetRevisions(typeof(SameIdTestEntity1), 1));
+			CollectionAssert.AreEquivalent(new[] { 1, 2 }, AuditReader().GetRevisions(typeof(SameIdTestEntity2), 1));
+		}
+
+		[Test]
+		public void VerifyHistoryOfSite1()
+		{
+			var ver1 = new SameIdTestEntity1 { Id = 1, Str1 = "str1" };
+			var ver2 = new SameIdTestEntity1 { Id = 1, Str1 = "str2" };
+
+			Assert.AreEqual(ver1, AuditReader().Find<SameIdTestEntity1>(1, 1));
+			Assert.AreEqual(ver2, AuditReader().Find<SameIdTestEntity1>(1, 2));
+		}
+
+		[Test]
+		public void VerifyHistoryOfSite2()
+		{
+			var ver1 = new SameIdTestEntity2 { Id = 1, Str1 = "str1" };
+			var ver2 = new SameIdTestEntity2 { Id = 1, Str1 = "str2" };
+
+			Assert.AreEqual(ver1, AuditReader().Find<SameIdTestEntity2>(1, 1));
+			Assert.AreEqual(ver2, AuditReader().Find<SameIdTestEntity2>(1, 2));
+		}
+	}
+}
