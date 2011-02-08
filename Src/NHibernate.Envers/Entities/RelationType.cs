e410da9088aed4a6f7cd8fada3e9913507b9e9c1
Fabio Maulo <fabiomaulo@gmail.com>
2011-02-08 14:47:25 -0300

    Minor (comments)

--- Src/NHibernate.Envers/Entities/RelationType.cs
+++ Src/NHibernate.Envers/Entities/RelationType.cs
@@ -1,38 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace NHibernate.Envers.Entities
+﻿namespace NHibernate.Envers.Entities
 {
-    /**
-     * Type of a relation between two entities.
-     * @author Simon Duduica, port of Envers omonyme class by Adam Warski (adam at warski dot org)
-    */
-    public enum RelationType
-    {
-        /**
-         * A single-reference-valued relation. The entity owns the relation.
-         */
-        ToOne,
-        /**
-         * A single-reference-valued relation. The entity doesn't own the relation. It is directly mapped in the related
-         * entity.
-         */
-        ToOneNotOwning,
-        /**
-         * A collection-of-references-valued relation. The entity doesn't own the relation. It is directly mapped in the
-         * related entity.
-         */
-        ToManyNotOwning,
-        /**
-         * A collection-of-references-valued relation. The entity owns the relation. It is mapped using a middle table.
-         */
-        ToManyMiddle,
-        /**
-         * A collection-of-references-valued relation. The entity doesn't own the relation. It is mapped using a middle
-         * table.
-         */
-        ToManyMiddleNotOwning
-    }
+	/// <summary>
+	/// Type of a relation between two entities.
+	/// </summary>
+	public enum RelationType
+	{
+		/// <summary>
+		/// A single-reference-valued relation. The entity owns the relation.
+		/// </summary>
+		ToOne,
+
+		/// <summary>
+		/// A single-reference-valued relation. The entity doesn't own the relation. It is directly mapped in the related entity.
+		/// </summary>
+		ToOneNotOwning,
+
+		/// <summary>
+		/// A collection-of-references-valued relation. The entity doesn't own the relation. It is directly mapped in the related entity.
+		/// </summary>
+		ToManyNotOwning,
+
+		/// <summary>
+		/// A collection-of-references-valued relation. The entity owns the relation. It is mapped using a middle table.
+		/// </summary>
+		ToManyMiddle,
+
+		/// <summary>
+		/// A collection-of-references-valued relation. The entity doesn't own the relation. It is mapped using a middle table.
+		/// </summary>
+		ToManyMiddleNotOwning
+	}
 }
