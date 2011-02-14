f48dc998773825c4bb4b626c251075e923f58cce
Roger Kratz <roger.kratz@gmail.com>
2011-02-14 14:07:50 +0100

    ienemerable instead of ienumerator + comments and other "java-ish -> c#-ish"

--- Src/NHibernate.Envers/Configuration/Metadata/ComponentMetadataGenerator.cs
+++ Src/NHibernate.Envers/Configuration/Metadata/ComponentMetadataGenerator.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using NHibernate.Mapping;
 using NHibernate.Envers.Configuration.Metadata.Reader;
 using NHibernate.Envers.Entities.Mapper;
@@ -9,43 +6,42 @@ using System.Xml;
 
 namespace NHibernate.Envers.Configuration.Metadata
 {
-    /**
-     * Generates metadata for components.
-     * @author Simon Duduica, port of Envers omonyme class by Adam Warski (adam at warski dot org)
-     */
-    public sealed class ComponentMetadataGenerator {
-	    private AuditMetadataGenerator mainGenerator;
+	/// <summary>
+	/// Generates metadata for components.
+	/// </summary>
+	public sealed class ComponentMetadataGenerator 
+	{
+		private readonly AuditMetadataGenerator mainGenerator;
 
-	    public ComponentMetadataGenerator(AuditMetadataGenerator auditMetadataGenerator) {
-		    mainGenerator = auditMetadataGenerator;
-	    }
+		public ComponentMetadataGenerator(AuditMetadataGenerator auditMetadataGenerator) 
+		{
+			mainGenerator = auditMetadataGenerator;
+		}
 
-	    //@SuppressWarnings({"unchecked"})
-	    public void AddComponent(XmlElement parent, PropertyAuditingData propertyAuditingData,
-							     IValue value, ICompositeMapperBuilder mapper, String entityName,
-							     EntityXmlMappingData xmlMappingData, bool firstPass) {
-		    Component prop_component = (Component) value;
+		public void AddComponent(XmlElement parent, PropertyAuditingData propertyAuditingData,
+								 IValue value, ICompositeMapperBuilder mapper, String entityName,
+								 EntityXmlMappingData xmlMappingData, bool firstPass) 
+		{
+			var prop_component = (Component) value;
 
-		    ICompositeMapperBuilder componentMapper = mapper.AddComponent(propertyAuditingData.GetPropertyData(),
-				    prop_component.ComponentClassName);
+			var componentMapper = mapper.AddComponent(propertyAuditingData.GetPropertyData(),
+					prop_component.ComponentClassName);
 
-		    // The property auditing data must be for a component.
-		    ComponentAuditingData componentAuditingData = (ComponentAuditingData) propertyAuditingData;
+			// The property auditing data must be for a component.
+			var componentAuditingData = (ComponentAuditingData) propertyAuditingData;
 
-		    // Adding all properties of the component
-		    IEnumerator<Property> properties = (IEnumerator<Property>) prop_component.PropertyIterator.GetEnumerator();
-		    while (properties.MoveNext()) {
-			    Property property = properties.Current;
+			// Adding all properties of the component
+			foreach (var property in prop_component.PropertyIterator)
+			{
+				var componentPropertyAuditingData = componentAuditingData.GetPropertyAuditingData(property.Name);
 
-			    PropertyAuditingData componentPropertyAuditingData =
-					    componentAuditingData.GetPropertyAuditingData(property.Name);
-
-			    // Checking if that property is audited
-			    if (componentPropertyAuditingData != null) {
-				    mainGenerator.AddValue(parent, property.Value, componentMapper, entityName, xmlMappingData,
-						    componentPropertyAuditingData, property.IsInsertable, firstPass);
-			    }
-		    }
-	    }
-    }
+				// Checking if that property is audited
+				if (componentPropertyAuditingData != null)
+				{
+					mainGenerator.AddValue(parent, property.Value, componentMapper, entityName, xmlMappingData,
+							componentPropertyAuditingData, property.IsInsertable, firstPass);
+				}
+			}
+		}
+	}
 }
