fa6bc34be5593f583e4eadb88384a8b91ecc7cc9
Fabio Maulo <fabiomaulo@gmail.com>
2011-02-14 20:04:42 -0300

    Minors

--- Src/NHibernate.Envers/Configuration/AuditConfiguration.cs
+++ Src/NHibernate.Envers/Configuration/AuditConfiguration.cs
@@ -9,23 +9,28 @@ using NHibernate.Envers.Synchronization;
 
 namespace NHibernate.Envers.Configuration
 {
-	public class AuditConfiguration 
+	public class AuditConfiguration
 	{
-		public AuditConfiguration(Cfg.Configuration cfg, IMetaDataProvider metaDataProvider) 
+		private static readonly IDictionary<Cfg.Configuration, AuditConfiguration> Configurations = new Dictionary<Cfg.Configuration, AuditConfiguration>();
+
+		private static readonly IDictionary<Cfg.Configuration, IMetaDataProvider> ConfigurationMetadataProvider = new Dictionary<Cfg.Configuration, IMetaDataProvider>();
+
+		protected AuditConfiguration(Cfg.Configuration cfg, IMetaDataProvider metaDataProvider)
 		{
 			var mds = new MetaDataStore(cfg, metaDataProvider);
 
-			var properties = cfg.Properties;
+			IDictionary<string, string> properties = cfg.Properties;
 			var propertyAndMemberInfo = new PropertyAndMemberInfo();
 			var revInfoCfg = new RevisionInfoConfiguration(mds, propertyAndMemberInfo); //rk - when configure other than attr, check here
-			var revInfoCfgResult = revInfoCfg.Configure(cfg); //rk - when configure other than attr, check here
+			RevisionInfoConfigurationResult revInfoCfgResult = revInfoCfg.Configure(cfg); //rk - when configure other than attr, check here
 			AuditEntCfg = new AuditEntitiesConfiguration(properties, revInfoCfgResult.RevisionInfoEntityName);
 			GlobalCfg = new GlobalConfiguration(properties);
 			AuditSyncManager = new AuditSyncManager(revInfoCfgResult.RevisionInfoGenerator);
 			RevisionInfoQueryCreator = revInfoCfgResult.RevisionInfoQueryCreator;
 			RevisionInfoNumberReader = revInfoCfgResult.RevisionInfoNumberReader;
-			EntCfg = new EntitiesConfigurator().Configure(cfg, mds, GlobalCfg, AuditEntCfg, //rk - when configure other than attr, check here (annotationmetadatareader & auditedpropertiesreader)
-					revInfoCfgResult.RevisionInfoXmlMapping, revInfoCfgResult.RevisionInfoRelationMapping, propertyAndMemberInfo);
+			EntCfg = new EntitiesConfigurator().Configure(cfg, mds, GlobalCfg, AuditEntCfg,
+			                                              //rk - when configure other than attr, check here (annotationmetadatareader & auditedpropertiesreader)
+			                                              revInfoCfgResult.RevisionInfoXmlMapping, revInfoCfgResult.RevisionInfoRelationMapping, propertyAndMemberInfo);
 		}
 
 		public virtual GlobalConfiguration GlobalCfg { get; private set; }
@@ -35,25 +40,21 @@ namespace NHibernate.Envers.Configuration
 		public virtual RevisionInfoQueryCreator RevisionInfoQueryCreator { get; private set; }
 		public virtual RevisionInfoNumberReader RevisionInfoNumberReader { get; private set; }
 
-		private static readonly IDictionary<Cfg.Configuration, AuditConfiguration> cfgs
-				= new Dictionary<Cfg.Configuration, AuditConfiguration>();
-
-		private static readonly IDictionary<Cfg.Configuration, IMetaDataProvider> confMetas
-				= new Dictionary<Cfg.Configuration, IMetaDataProvider>();
-
 
 		[MethodImpl(MethodImplOptions.Synchronized)]
-		public static AuditConfiguration GetFor(Cfg.Configuration cfg) 
+		public static AuditConfiguration GetFor(Cfg.Configuration cfg)
 		{
 			AuditConfiguration verCfg;
-			if (!cfgs.TryGetValue(cfg, out verCfg))
+			if (!Configurations.TryGetValue(cfg, out verCfg))
 			{
 				IMetaDataProvider metas;
-				if (!confMetas.TryGetValue(cfg, out metas))
+				if (!ConfigurationMetadataProvider.TryGetValue(cfg, out metas))
+				{
 					metas = new AttributeConfiguration();
+				}
 				verCfg = new AuditConfiguration(cfg, metas);
-				cfgs.Add(cfg, verCfg);
-				
+				Configurations.Add(cfg, verCfg);
+
 				cfg.BuildMappings();
 			}
 
@@ -63,7 +64,7 @@ namespace NHibernate.Envers.Configuration
 		[MethodImpl(MethodImplOptions.Synchronized)]
 		public static void SetConfigMetas(Cfg.Configuration cfg, IMetaDataProvider metaDataProvider)
 		{
-			confMetas[cfg] = metaDataProvider;
+			ConfigurationMetadataProvider[cfg] = metaDataProvider;
 		}
 	}
 }
