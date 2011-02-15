179e8f2f85c28baf193125f5e51429a7894fcfe7
Fabio Maulo <fabiomaulo@gmail.com>
2011-02-15 12:06:12 -0300

    Minor: added comments to IntegrateWithEnvers

--- Src/NHibernate.Envers/Configuration/NhConfigurationExtension.cs
+++ Src/NHibernate.Envers/Configuration/NhConfigurationExtension.cs
@@ -8,9 +8,18 @@ namespace NHibernate.Cfg
 {
 	public static class NhConfigurationExtension
 	{
-		public static Configuration IntegrateWithEnvers(this Configuration configuration, 
-														AuditEventListener auditEventListener,
-														IMetaDataProvider metaDataProvider)
+		/// <summary>
+		/// Integrate Envers with NHibernate.
+		/// </summary>
+		/// <param name="configuration">The NHibernate configuration.</param>
+		/// <param name="auditEventListener">The custom implementation for all listeners (better if inherited from <see cref="AuditEventListener"/>>)</param>
+		/// <param name="metaDataProvider">The provider of metadata (attributes, embedded fluent-configuration or custom <see cref="IMetaDataProvider"/> for custom DSL.</param>
+		/// <returns>The NHibernate configuration.</returns>
+		/// <remarks>
+		/// WARNING: Be sure to call this method after set all configuration properties, after have added all your mappings 
+		/// and after integrate NHibernate with all others packages as NHibernate.Validator, NHibernate.Search, NHibernate.Spatial.
+		/// </remarks>
+		public static Configuration IntegrateWithEnvers(this Configuration configuration, AuditEventListener auditEventListener, IMetaDataProvider metaDataProvider)
 		{
 			AuditConfiguration.SetConfigMetas(configuration, metaDataProvider);
 			AddListeners(configuration, auditEventListener);
@@ -26,6 +35,8 @@ namespace NHibernate.Cfg
 		/// <returns>The NHibernate configuration.</returns>
 		/// <remarks>
 		/// The default <see cref="AuditEventListener"/> will be used.
+		/// WARNING: Be sure to call this method after set all configuration properties, after have added all your mappings 
+		/// and after integrate NHibernate with all others packages as NHibernate.Validator, NHibernate.Search, NHibernate.Spatial.
 		/// </remarks>
 		/// <seealso cref="AttributeConfiguration"/>
 		/// <seealso cref="NHibernate.Envers.Configuration.Fluent.FluentConfiguration"/>
@@ -41,6 +52,8 @@ namespace NHibernate.Cfg
 		/// <returns>The NHibernate configuration.</returns>
 		/// <remarks>
 		/// The default <see cref="AuditEventListener"/> and the <see cref="AttributeConfiguration"/> will be used.
+		/// WARNING: Be sure to call this method after set all configuration properties, after have added all your mappings 
+		/// and after integrate NHibernate with all others packages as NHibernate.Validator, NHibernate.Search, NHibernate.Spatial.
 		/// </remarks>
 		public static Configuration IntegrateWithEnvers(this Configuration configuration)
 		{
