4f74ced62b18ff19ce3dc88350d1e6b1e44e7d18
Roger Kratz <roger.kratz@gmail.com>
2011-02-18 12:16:47 +0100

    comments

--- Src/NHibernate.Envers/Configuration/Store/IEntityMeta.cs
+++ Src/NHibernate.Envers/Configuration/Store/IEntityMeta.cs
@@ -5,11 +5,18 @@ using System.Reflection;
 namespace NHibernate.Envers.Configuration.Store
 {
 	/// <summary>
-	/// Holds configuration information for an entity
+	/// Holds configuration information for an entity type
 	/// </summary>
 	public interface IEntityMeta
 	{
+		/// <summary>
+		/// Metas for current class
+		/// </summary>
 		IEnumerable<Attribute> ClassMetas { get; }
+
+		/// <summary>
+		/// Member metas for current class
+		/// </summary>
 		IDictionary<MemberInfo, IEnumerable<Attribute>> MemberMetas { get; }
 	}
 }
