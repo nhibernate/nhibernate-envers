48dfadc450597d90bc6e2755013729913a1a7f27
Roger Kratz <roger.kratz@gmail.com>
2011-02-18 13:40:45 +0100

    cleaning up

--- Src/NHibernate.Envers/Synchronization/Work/IWorkUnitMergeVisitor.cs
+++ Src/NHibernate.Envers/Synchronization/Work/IWorkUnitMergeVisitor.cs
@@ -3,12 +3,12 @@
 	/// <summary>
 	/// Visitor pattern visitor. All methods should be invoked on the first work unit.
 	/// </summary>
-    public interface IWorkUnitMergeVisitor
-    {
-        IAuditWorkUnit Merge(AddWorkUnit second);
-        IAuditWorkUnit Merge(ModWorkUnit second);
-        IAuditWorkUnit Merge(DelWorkUnit second);
-        IAuditWorkUnit Merge(CollectionChangeWorkUnit second);
-        IAuditWorkUnit Merge(FakeBidirectionalRelationWorkUnit second);
-    }
+	public interface IWorkUnitMergeVisitor
+	{
+		IAuditWorkUnit Merge(AddWorkUnit second);
+		IAuditWorkUnit Merge(ModWorkUnit second);
+		IAuditWorkUnit Merge(DelWorkUnit second);
+		IAuditWorkUnit Merge(CollectionChangeWorkUnit second);
+		IAuditWorkUnit Merge(FakeBidirectionalRelationWorkUnit second);
+	}
 }
