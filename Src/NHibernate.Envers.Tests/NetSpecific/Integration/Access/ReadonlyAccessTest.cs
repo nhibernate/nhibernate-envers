171e28f91d57e16c81649826e9d943db42826f08
Roger Kratz <roger.kratz@gmail.com>
2011-02-18 11:57:26 +0100

    test for readonly access

---
+++ Src/NHibernate.Envers.Tests/NetSpecific/Integration/Access/ReadonlyAccessTest.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using NHibernate.Envers.Query;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace NHibernate.Envers.Tests.NetSpecific.Integration.Access
+{
+	[TestFixture]
+	public class ReadonlyAccessTest : TestBase
+	{
+		private int id;
+
+		protected override void Initialize()
+		{
+			var ent = new ReadonlyEntity{Data=1};
+			ent.SetReadOnlyData(1);
+
+			using (var tx = Session.BeginTransaction())
+			{
+				id = (int) Session.Save(ent);
+				tx.Commit();
+			}
+			using (var tx = Session.BeginTransaction())
+			{
+				ent.Data = 2;
+				ent.SetReadOnlyData(2);
+				tx.Commit();
+			}
+		}
+
+		[Test]
+		public void VerifyRevisionCounts()
+		{
+			CollectionAssert.AreEqual(new[] { 1, 2 }, AuditReader().GetRevisions(typeof(ReadonlyEntity), id));
+		}
+
+		[Test]
+		public void VerifyHistoryOfComponent()
+		{
+			var ver1 = AuditReader().Find<ReadonlyEntity>(id, 1);
+			var ver2 = AuditReader().Find<ReadonlyEntity>(id, 2);
+
+			ver1.Data.Should().Be.EqualTo(1);
+			ver1.ReadonlyData.Should().Be.EqualTo(0);
+
+			ver2.Data.Should().Be.EqualTo(2);
+			ver2.ReadonlyData.Should().Be.EqualTo(0);
+		}
+
+		[Test]
+		public void CanQueryOnReadonlyData()
+		{
+			var res = (IList)AuditReader().CreateQuery()
+				.ForRevisionsOfEntity(typeof (ReadonlyEntity), false, false)
+				.Add(AuditEntity.Property("ReadonlyData").Eq(2))
+				.GetSingleResult();
+
+			var ent = (ReadonlyEntity) res[0];
+			var rev = (DefaultRevisionEntity) res[1];
+
+			rev.Id.Should().Be.EqualTo(2);
+			ent.Data.Should().Be.EqualTo(2);
+			ent.ReadonlyData.Should().Be.EqualTo(0);
+		}
+	}
+}
---
+++ Src/NHibernate.Envers.Tests/NetSpecific/Integration/Access/ReadonlyEntity.cs
@@ -0,0 +1,22 @@
+namespace NHibernate.Envers.Tests.NetSpecific.Integration.Access
+{
+	[Audited]
+	public class ReadonlyEntity
+	{
+		private int _readonlyData;
+		public virtual int Id { get; set; }
+		public virtual int Data { get; set; }
+		public virtual int ReadonlyData 
+		{
+			get
+			{
+				return _readonlyData;
+			}
+		}
+
+		public virtual void SetReadOnlyData(int value)
+		{
+			_readonlyData = value;
+		}
+	}
+}
