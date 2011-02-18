f1cb9ac9d683413c3e3f4a360b9e41153a467762
Roger Kratz <roger.kratz@gmail.com>
2011-02-18 13:55:29 +0100

    removing tests about multid. No need to keep them now - not supported in NH Core

--- Src/NHibernate.Envers/Reader/IAuditReaderImplementor.cs
+++ Src/NHibernate.Envers/Reader/IAuditReaderImplementor.cs
@@ -1,18 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using NHibernate.Engine;
+﻿using NHibernate.Engine;
 
 namespace NHibernate.Envers.Reader
 {
-    /**
-     * An interface exposed by a VersionsReader to library-facing classes.
-     * @author Adam Warski (adam at warski dot org)
-     */
-    public interface IAuditReaderImplementor : IAuditReader {
-        ISessionImplementor SessionImplementor { get; }
-        ISession Session { get; }
-        IFirstLevelCache FirstLevelCache { get; }
-    }
+	/// <summary>
+	/// An interface exposed by a VersionsReader to library-facing classes.
+	/// </summary>
+	public interface IAuditReaderImplementor : IAuditReader 
+	{
+		ISessionImplementor SessionImplementor { get; }
+		ISession Session { get; }
+		IFirstLevelCache FirstLevelCache { get; }
+	}
 }
