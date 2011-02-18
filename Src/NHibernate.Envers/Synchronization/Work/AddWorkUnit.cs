2b4d2131196971aa6e254dd1b3ab7a364d6c526e
Roger Kratz <roger.kratz@gmail.com>
2011-02-18 13:40:45 +0100

    cleaning up

--- Src/NHibernate.Envers/Synchronization/Work/AddWorkUnit.cs
+++ Src/NHibernate.Envers/Synchronization/Work/AddWorkUnit.cs
@@ -1,74 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using NHibernate.Engine;
 using NHibernate.Envers.Configuration;
 using NHibernate.Persister.Entity;
 
 namespace NHibernate.Envers.Synchronization.Work
 {
-    /**
-     * @author Simon Duduica, port of Envers omonyme class by Adam Warski (adam at warski dot org)
-     */
-    public class AddWorkUnit : AbstractAuditWorkUnit, IAuditWorkUnit {
-    private readonly IDictionary<String, Object> data;
+	public class AddWorkUnit : AbstractAuditWorkUnit, IAuditWorkUnit 
+	{
+		private readonly IDictionary<string, object> data;
 
-    public AddWorkUnit(ISessionImplementor sessionImplementor, String entityName, AuditConfiguration verCfg,
-					   Object id, IEntityPersister entityPersister, Object[] state) 
-        : base(sessionImplementor, entityName, verCfg, id)
-    {
-        data = new Dictionary<String, Object>();
-        verCfg.EntCfg[EntityName].PropertyMapper.Map(sessionImplementor, data,
-				entityPersister.PropertyNames, state, null);
-    }
+		public AddWorkUnit(ISessionImplementor sessionImplementor, string entityName, AuditConfiguration verCfg,
+						   object id, IEntityPersister entityPersister, object[] state) 
+			: base(sessionImplementor, entityName, verCfg, id)
+		{
+			data = new Dictionary<string, object>();
+			verCfg.EntCfg[EntityName].PropertyMapper.Map(sessionImplementor, data,
+					entityPersister.PropertyNames, state, null);
+		}
 
-    public AddWorkUnit(ISessionImplementor sessionImplementor, String entityName, AuditConfiguration verCfg,
-                       Object id, IDictionary<String, Object> data)
-        : base(sessionImplementor, entityName, verCfg, id)
-    {
-        this.data = data;
-    }
+		public AddWorkUnit(ISessionImplementor sessionImplementor, string entityName, AuditConfiguration verCfg,
+						   object id, IDictionary<string, object> data)
+			: base(sessionImplementor, entityName, verCfg, id)
+		{
+			this.data = data;
+		}
 
-    public override bool ContainsWork()
-    {
-        return true;
-    }
+		public override bool ContainsWork()
+		{
+			return true;
+		}
 
-    public override IDictionary<String, Object> GenerateData(Object revisionData)
-    {
-        FillDataWithId(data, revisionData, RevisionType.Added);
-        return data;
-    }
+		public override IDictionary<string, object> GenerateData(object revisionData)
+		{
+			FillDataWithId(data, revisionData, RevisionType.Added);
+			return data;
+		}
 
-    public override IAuditWorkUnit Merge(AddWorkUnit second)
-    {
-        return second;
-    }
+		public override IAuditWorkUnit Merge(AddWorkUnit second)
+		{
+			return second;
+		}
 
-    public override IAuditWorkUnit Merge(ModWorkUnit second)
-    {
-        return new AddWorkUnit(sessionImplementor, EntityName, verCfg, EntityId, second.getData());
-    }
+		public override IAuditWorkUnit Merge(ModWorkUnit second)
+		{
+			return new AddWorkUnit(sessionImplementor, EntityName, verCfg, EntityId, second.Data);
+		}
 
-    public override IAuditWorkUnit Merge(DelWorkUnit second)
-    {
-        return null;
-    }
+		public override IAuditWorkUnit Merge(DelWorkUnit second)
+		{
+			return null;
+		}
 
-    public override IAuditWorkUnit Merge(CollectionChangeWorkUnit second)
-    {
-        return this;
-    }
+		public override IAuditWorkUnit Merge(CollectionChangeWorkUnit second)
+		{
+			return this;
+		}
 
-    public override IAuditWorkUnit Merge(FakeBidirectionalRelationWorkUnit second)
-    {
-        return FakeBidirectionalRelationWorkUnit.Merge(second, this, second.NestedWorkUnit);
-    }
+		public override IAuditWorkUnit Merge(FakeBidirectionalRelationWorkUnit second)
+		{
+			return FakeBidirectionalRelationWorkUnit.Merge(second, this, second.NestedWorkUnit);
+		}
 
-    public override IAuditWorkUnit Dispatch(IWorkUnitMergeVisitor first)
-    {
-        return first.Merge(this);
-    }
-}
+		public override IAuditWorkUnit Dispatch(IWorkUnitMergeVisitor first)
+		{
+			return first.Merge(this);
+		}
+	}
 }
