329cd6c8af10e347eaf5c266b9e2f02793efd24a
Roger Kratz <roger.kratz@gmail.com>
2011-02-25 11:06:40 +0100

    minor

--- Src/NHibernate.Envers/RelationTargetAuditMode.cs
+++ Src/NHibernate.Envers/RelationTargetAuditMode.cs
@@ -1,8 +1,5 @@
 ﻿namespace NHibernate.Envers
 {
-    /**
-     * @author Simon Duduica, port of Envers omonyme class by Adam Warski (adam at warski dot org)
-     */
     public enum RelationTargetAuditMode
     {
         Audited,
