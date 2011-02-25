1d8444fa2fcb4f2ee28e75188db99c8c34975f12
Roger Kratz <roger.kratz@gmail.com>
2011-02-25 10:58:47 +0100

    moved revisioninfoconfigurationresult to a separate file

--- Src/NHibernate.Envers/Configuration/RevisionInfoConfiguration.cs
+++ Src/NHibernate.Envers/Configuration/RevisionInfoConfiguration.cs
@@ -232,28 +232,4 @@ namespace NHibernate.Envers.Configuration
 			return type.Equals(typeof(DateTime));
 		}
 	}
-
-	public class RevisionInfoConfigurationResult 
-	{
-		public RevisionInfoConfigurationResult(IRevisionInfoGenerator revisionInfoGenerator,
-										XmlDocument revisionInfoXmlMapping, 
-										RevisionInfoQueryCreator revisionInfoQueryCreator,
-										XmlElement revisionInfoRelationMapping,
-										RevisionInfoNumberReader revisionInfoNumberReader, 
-										string revisionInfoEntityName) {
-			RevisionInfoGenerator = revisionInfoGenerator;
-			RevisionInfoXmlMapping = revisionInfoXmlMapping;
-			RevisionInfoQueryCreator = revisionInfoQueryCreator;
-			RevisionInfoRelationMapping = revisionInfoRelationMapping;
-			RevisionInfoNumberReader = revisionInfoNumberReader;
-			RevisionInfoEntityName = revisionInfoEntityName;
-		}
-
-		public IRevisionInfoGenerator RevisionInfoGenerator { get; private set; }
-		public XmlDocument RevisionInfoXmlMapping { get; private set; }
-		public RevisionInfoQueryCreator RevisionInfoQueryCreator { get; private set; }
-		public XmlElement RevisionInfoRelationMapping { get; private set; }
-		public RevisionInfoNumberReader RevisionInfoNumberReader { get; private set; }
-		public string RevisionInfoEntityName { get; private set; }
-	}
 }
---
+++ Src/NHibernate.Envers/Configuration/RevisionInfoConfigurationResult.cs
@@ -0,0 +1,29 @@
+﻿using System.Xml;
+using NHibernate.Envers.RevisionInfo;
+
+namespace NHibernate.Envers.Configuration
+{
+	public class RevisionInfoConfigurationResult 
+	{
+		public RevisionInfoConfigurationResult(IRevisionInfoGenerator revisionInfoGenerator,
+		                                       XmlDocument revisionInfoXmlMapping, 
+		                                       RevisionInfoQueryCreator revisionInfoQueryCreator,
+		                                       XmlElement revisionInfoRelationMapping,
+		                                       RevisionInfoNumberReader revisionInfoNumberReader, 
+		                                       string revisionInfoEntityName) {
+			RevisionInfoGenerator = revisionInfoGenerator;
+			RevisionInfoXmlMapping = revisionInfoXmlMapping;
+			RevisionInfoQueryCreator = revisionInfoQueryCreator;
+			RevisionInfoRelationMapping = revisionInfoRelationMapping;
+			RevisionInfoNumberReader = revisionInfoNumberReader;
+			RevisionInfoEntityName = revisionInfoEntityName;
+		                                       }
+
+		public IRevisionInfoGenerator RevisionInfoGenerator { get; private set; }
+		public XmlDocument RevisionInfoXmlMapping { get; private set; }
+		public RevisionInfoQueryCreator RevisionInfoQueryCreator { get; private set; }
+		public XmlElement RevisionInfoRelationMapping { get; private set; }
+		public RevisionInfoNumberReader RevisionInfoNumberReader { get; private set; }
+		public string RevisionInfoEntityName { get; private set; }
+	}
+}
