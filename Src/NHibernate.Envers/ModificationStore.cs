99ac584a15ecf2269d2f26c87a6aca1e42779d43
Roger Kratz <roger.kratz@gmail.com>
2011-02-25 11:06:40 +0100

    minor

--- Src/NHibernate.Envers/ModificationStore.cs
+++ Src/NHibernate.Envers/ModificationStore.cs
@@ -1,8 +1,5 @@
 ﻿namespace NHibernate.Envers
 {
-    /**
-     * @author Simon Duduica, port of Envers omonyme class by Adam Warski (adam at warski dot org)
-     */
     public enum ModificationStore
     {
         None, // C# does not accept null values passed for an enum value so we use this.
