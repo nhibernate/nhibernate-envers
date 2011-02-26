4e889d4b0f9eb7ad3f52bc89c695f42684b517a4
Fabio Maulo <fabiomaulo@gmail.com>
2011-02-26 10:56:17 -0300

    Detailed result of a query over the history of an entity.

---
+++ Src/NHibernate.Envers/Query/IRevisionEntityInfo.cs
@@ -0,0 +1,15 @@
+namespace NHibernate.Envers.Query
+{
+	/// <summary>
+	/// Detailed result of a query over the history of an entity. 
+	/// </summary>
+	/// <typeparam name="TEntity">The type of the entity.</typeparam>
+	/// <typeparam name="TRevisionEntity">The type of RevisionEntity</typeparam>
+	/// <seealso cref="DefaultRevisionEntity"/>
+	public interface IRevisionEntityInfo<TEntity, TRevisionEntity>
+	{
+		TEntity Entity { get; }
+		TRevisionEntity RevisionEntity { get; }
+		RevisionType Operation { get; }
+	}
+}
