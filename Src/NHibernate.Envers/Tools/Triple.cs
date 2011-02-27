050fe908b50ebdaed29f12c4777846e7d64dd163
Fabio Maulo <fabiomaulo@gmail.com>
2011-02-27 16:14:20 -0300

    Minor

--- Src/NHibernate.Envers/Tools/Triple.cs
+++ Src/NHibernate.Envers/Tools/Triple.cs
@@ -1,43 +1,64 @@
 ﻿namespace NHibernate.Envers.Tools
 {
-    public class Triple<T1, T2, T3> 
+	public class Triple<T1, T2, T3>
 	{
-        public T1 First { get; private set; }
-        public T2 Second { get; private set; }
-        public T3 Third { get; private set; }
+		private readonly int hashCode;
 
-        public Triple(T1 First, T2 Second, T3 Third) 
+		internal Triple(T1 first, T2 second, T3 third)
 		{
-            this.First = First;
-            this.Second = Second;
-            this.Third = Third;
-        }
+			First = first;
+			Second = second;
+			Third = third;
+			unchecked
+			{
+				hashCode = ReferenceEquals(First, null) ? 17 : First.GetHashCode();
+				hashCode = (hashCode*397) ^ (ReferenceEquals(Second, null) ? 19 : Second.GetHashCode());
+				hashCode = (hashCode*397) ^ (ReferenceEquals(Third, null) ? 23 : Third.GetHashCode());
+			}
+		}
 
-        public override bool Equals(object o) 
+		public T1 First { get; private set; }
+		public T2 Second { get; private set; }
+		public T3 Third { get; private set; }
+
+		public override bool Equals(object obj)
 		{
-            if (this == o) return true;
-            if (!(o is Triple<T1, T2, T3>)) return false;
+			if (ReferenceEquals(null, obj))
+			{
+				return false;
+			}
+			if (ReferenceEquals(this, obj))
+			{
+				return true;
+			}
+			if (obj.GetType() != typeof (Triple<T1, T2, T3>))
+			{
+				return false;
+			}
+			return Equals((Triple<T1, T2, T3>) obj);
+		}
 
-            var triple = (Triple<T1, T2, T3>) o;
-
-            if (First != null ? !First.Equals(triple.First) : triple.First != null) return false;
-            if (Second != null ? !Second.Equals(triple.Second) : triple.Second != null) return false;
-            if (Third != null ? !Third.Equals(triple.Third) : triple.Third != null) return false;
-
-            return true;
-        }
-
-        public override int GetHashCode() 
+		public static Triple<T1, T2, T3> Make(T1 first, T2 second, T3 third)
 		{
-        	int result = (First != null ? First.GetHashCode() : 0);
-            result = 31 * result + (Second != null ? Second.GetHashCode() : 0);
-            result = 31 * result + (Third != null ? Third.GetHashCode() : 0);
-            return result;
-        }
+			return new Triple<T1, T2, T3>(first, second, third);
+		}
 
-        public static Triple<T1, T2, T3> Make(T1 First, T2 Second, T3 Third)
-        {
-            return new Triple<T1, T2, T3>(First, Second, Third);
-        }
-    }
+		public bool Equals(Triple<T1, T2, T3> other)
+		{
+			if (ReferenceEquals(null, other))
+			{
+				return false;
+			}
+			if (ReferenceEquals(this, other))
+			{
+				return true;
+			}
+			return Equals(other.First, First) && Equals(other.Second, Second) && Equals(other.Third, Third);
+		}
+
+		public override int GetHashCode()
+		{
+			return hashCode;
+		}
+	}
 }
