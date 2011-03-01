68f874d550c59a55d3441371a4416a60db39aa6d
Roger Kratz <roger.kratz@gmail.com>
2011-03-01 22:58:24 +0100

    compile warnings

--- Src/NHibernate.Envers.Tests/NetSpecific/UnitTests/Fluent/Model/Inheritance.cs
+++ Src/NHibernate.Envers.Tests/NetSpecific/UnitTests/Fluent/Model/Inheritance.cs
@@ -2,7 +2,10 @@ namespace NHibernate.Envers.Tests.NetSpecific.UnitTests.Fluent.Model
 {
 	public class Animal
 	{
+#pragma warning disable 0169
 		private int weight;
+#pragma warning restore 0169
+
 		public string Name { get; set; }
 	}
 
