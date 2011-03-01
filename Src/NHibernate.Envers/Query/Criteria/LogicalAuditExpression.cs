694e4086f0d031d738443f0069083abeba39d3a4
Roger Kratz <roger.kratz@gmail.com>
2011-03-01 22:31:50 +0100

    clean up

--- Src/NHibernate.Envers/Query/Criteria/LogicalAuditExpression.cs
+++ Src/NHibernate.Envers/Query/Criteria/LogicalAuditExpression.cs
@@ -1,28 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using NHibernate.Envers.Configuration;
 using NHibernate.Envers.Tools.Query;
 
 namespace NHibernate.Envers.Query.Criteria
 {
-    /**
-     * @author Adam Warski (adam at warski dot org)
-     */
-    public class LogicalAuditExpression : IAuditCriterion {
-        private IAuditCriterion lhs;
-        private IAuditCriterion rhs;
-        private String op;
+    public class LogicalAuditExpression : IAuditCriterion 
+	{
+        private readonly IAuditCriterion lhs;
+        private readonly IAuditCriterion rhs;
+        private readonly string op;
 
-        public LogicalAuditExpression(IAuditCriterion lhs, IAuditCriterion rhs, String op) {
+        public LogicalAuditExpression(IAuditCriterion lhs, IAuditCriterion rhs, string op) 
+		{
             this.lhs = lhs;
             this.rhs = rhs;
             this.op = op;
         }
 
-        public void AddToQuery(AuditConfiguration verCfg, String entityName, QueryBuilder qb, Parameters parameters) {
-            Parameters opParameters = parameters.AddSubParameters(op);
+        public void AddToQuery(AuditConfiguration verCfg, String entityName, QueryBuilder qb, Parameters parameters) 
+		{
+            var opParameters = parameters.AddSubParameters(op);
 
             lhs.AddToQuery(verCfg, entityName, qb, opParameters.AddSubParameters("and"));
             rhs.AddToQuery(verCfg, entityName, qb, opParameters.AddSubParameters("and"));
