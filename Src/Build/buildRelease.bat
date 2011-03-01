47581bee3ebb40acd5c15eb8f186f01deedf299d
Roger Kratz <roger.kratz@gmail.com>
2011-03-01 11:19:50 +0100

    moved build scripts to build folder. removed remarked configuration info

--- Src/Build/buildRelease.bat
+++ Src/Build/buildRelease.bat
@@ -2,7 +2,7 @@
 SET sn="%programfiles%\Microsoft SDKs\Windows\V6.0A\Bin\sn.exe"
 set gacutil="%programfiles%\Microsoft SDKs\Windows\V6.0A\Bin\gacutil.exe"
 set msbuild="%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
-REM set configuration=Release
+set configuration=Release
 %msbuild% default.msbuild /v:n /t:Build
 echo -------------------------------
 pause
