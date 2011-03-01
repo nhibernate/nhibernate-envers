18959ab74935042450fc45cc990cc474cf890273
Roger Kratz <roger.kratz@gmail.com>
2011-03-01 14:02:08 +0100

    clean up

--- Src/NHibernate.Envers/Query/Property/EntityPropertyName.cs
+++ Src/NHibernate.Envers/Query/Property/EntityPropertyName.cs
@@ -1,24 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using NHibernate.Envers.Configuration;
+﻿using NHibernate.Envers.Configuration;
 
 namespace NHibernate.Envers.Query.Property
 {
-    /**
-     * Used for specifying restrictions on a property of an audited entity.
-     * @author Adam Warski (adam at warski dot org)
-     */
-    public class EntityPropertyName : IPropertyNameGetter {
-        private readonly String propertyName;
+	/// <summary>
+	/// Used for specifying restrictions on a property of an audited entity.
+	/// </summary>
+	public class EntityPropertyName : IPropertyNameGetter 
+	{
+		private readonly string propertyName;
 
-        public EntityPropertyName(String propertyName) {
-            this.propertyName = propertyName;
-        }
+		public EntityPropertyName(string propertyName) 
+		{
+			this.propertyName = propertyName;
+		}
 
-        public String Get(AuditConfiguration auditCfg) {
-            return propertyName;
-        }
-    }
+		public string Get(AuditConfiguration auditCfg) 
+		{
+			return propertyName;
+		}
+	}
 }
