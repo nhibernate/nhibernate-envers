6f2297ff57ef55ceae13572a16fab7dfae251a17
Roger Kratz <roger.kratz@gmail.com>
2011-03-01 22:58:24 +0100

    compile warnings

--- Src/NHibernate.Envers.Tests/NetSpecific/UnitTests/Fluent/Model/FieldEntity.cs
+++ Src/NHibernate.Envers.Tests/NetSpecific/UnitTests/Fluent/Model/FieldEntity.cs
@@ -2,7 +2,10 @@ namespace NHibernate.Envers.Tests.NetSpecific.UnitTests.Fluent.Model
 {
 	public class FieldEntity
 	{
+#pragma warning disable 0169 
 		private object data1;
+#pragma warning restore 0169
+
 		public object data2 { get; set; }
 	}
 }
