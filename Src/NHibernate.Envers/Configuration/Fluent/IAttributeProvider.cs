db0540c65e001104a558f83fc92ab312be87213e
Roger Kratz <roger.kratz@gmail.com>
2011-03-02 20:52:17 +0100

    comments

--- Src/NHibernate.Envers/Configuration/Fluent/IAttributeProvider.cs
+++ Src/NHibernate.Envers/Configuration/Fluent/IAttributeProvider.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 
 namespace NHibernate.Envers.Configuration.Fluent
 {
+	/// <summary>
+	/// Creates attributes based on fluent configuration.
+	/// </summary>
 	public interface IAttributeProvider
 	{
+		/// <summary>
+		/// The entity type this configuration is for
+		/// </summary>
 		System.Type Type { get; }
+
+		/// <summary>
+		/// Creates corresponding class attributes
+		/// </summary>
+		/// <returns>The attributes</returns>
 		IEnumerable<Attribute> CreateClassAttributes();
+
+		/// <summary>
+		/// Creates corresponding member attributes
+		/// </summary>
+		/// <returns>The attributes</returns>
 		IEnumerable<MemberInfoAndAttribute> CreateMemberAttributes();
 	}
 }
