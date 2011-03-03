f95e6911e18d8ae196d15e3e26c4fda2b5ff1cf3
Roger Kratz <roger.kratz@gmail.com>
2011-03-03 16:55:19 +0100

    failing test (set to ignore for now) for NHE7

---
+++ Src/NHibernate.Envers.Tests/NetSpecific/Integration/Transaction/RollbackAuditExceptionTest.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using NHibernate.Envers.Tests.Entities;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace NHibernate.Envers.Tests.NetSpecific.Integration.Transaction
+{
+	[TestFixture, Ignore("Fails right now - NHE-7")]
+	public class RollbackAuditExceptionTest : TestBase
+	{
+		protected override void Initialize()
+		{
+			dropStrTestAuditTable();
+		}
+
+		[Test]
+		public void WhenAuditPeristExceptionOccursTransactionShouldBeRolledBack()
+		{
+			int intId;
+			var strEntity = new StrTestEntity();
+			var intEntity = new IntTestEntity();
+
+			using (var tx = Session.BeginTransaction())
+			{
+				intId = (int)Session.Save(intEntity);
+				Session.Save(strEntity);
+				Assert.Throws<NullReferenceException>(tx.Commit);
+			}
+			verifyNoDataGotPeristed(intId);
+		}
+
+		[Test]
+		public void WhenAuditPeristExceptionOccursTransactionShouldBeRolledBack_FlushModeNever()
+		{
+			int intId;
+			var strEntity = new StrTestEntity();
+			var intEntity = new IntTestEntity();
+			Session.FlushMode = FlushMode.Never;
+
+			using (var tx = Session.BeginTransaction())
+			{
+				intId = (int)Session.Save(intEntity);
+				Session.Save(strEntity);
+				Assert.Throws<NullReferenceException>(tx.Commit);
+			}
+			verifyNoDataGotPeristed(intId);
+		}
+
+		private void verifyNoDataGotPeristed(int id)
+		{
+			using(Session.BeginTransaction())
+			{
+				Session.CreateQuery("select count(s) from StrTestEntity s ").UniqueResult<long>()
+					.Should().Be.EqualTo(0);
+				Session.CreateQuery("select count(s) from IntTestEntity s ").UniqueResult<long>()
+					.Should().Be.EqualTo(0);			
+				Session.Auditer().GetRevisions<IntTestEntity>(id)
+					.Should().Be.Empty();
+			}
+		}
+
+		private void dropStrTestAuditTable()
+		{
+			Session.CreateSQLQuery("drop table StrTestEntity_AUD").ExecuteUpdate();
+		}
+
+
+		protected override IEnumerable<string> Mappings
+		{
+			get
+			{
+				return new[] { "Entities.Mapping.hbm.xml"};
+			}
+		}
+	}
+}
