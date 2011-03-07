83d33633a69c092b41074b870f72de6ed56a39eb
Roger Kratz <roger.kratz@gmail.com>
2011-03-07 23:09:13 +0100

    minor

--- Src/NHibernate.Envers/Tools/Graph/Vertex.cs
+++ Src/NHibernate.Envers/Tools/Graph/Vertex.cs
@@ -2,25 +2,28 @@
 
 namespace NHibernate.Envers.Tools.Graph
 {
-    /**
-     * A graph vertex - stores its representation, neighbours, start and end time in (D|B)FS.
- * @author Simon Duduica, port of Envers omonyme class by Adam Warski (adam at warski dot org)
-     */
-    public class Vertex<R> {
-        public R Representation { get; private set; }
+	/// <summary>
+	/// A graph vertex - stores its representation, neighbours, start and end time in (D|B)FS.
+	/// </summary>
+	/// <typeparam name="R"></typeparam>
+    public class Vertex<R> 
+	{
+		public Vertex(R representation)
+		{
+			Representation = representation;
+			Neighbours = new List<Vertex<R>>();
+			StartTime = 0;
+			EndTime = 0;
+		}
+
+		public R Representation { get; private set; }
         public IList<Vertex<R>> Neighbours { get; private set; }
 
         public int StartTime { get; set; }
         public int EndTime { get; set; }
 
-        public Vertex(R representation) {
-            this.Representation = representation;
-            this.Neighbours = new List<Vertex<R>>();
-            this.StartTime = 0;
-            this.EndTime = 0;
-        }
-
-        public void addNeighbour(Vertex<R> n) {
+        public void addNeighbour(Vertex<R> n) 
+		{
             Neighbours.Add(n);
         }
     }
