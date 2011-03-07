928b196cf24faec3828ff8b8e46e284786595e64
Roger Kratz <roger.kratz@gmail.com>
2011-03-07 23:09:41 +0100

    removed dead code

--- Src/NHibernate.Envers/Configuration/Fluent/IMainConfiguration.cs
+++
@@ -1,9 +0,0 @@
-using NHibernate.Envers.Configuration.Store;
-
-namespace NHibernate.Envers.Configuration.Fluent
-{
-	public interface IMainConfiguration
-	{
-		IMetaDataProvider BuildMetaDataProvider();
-	}
-}
