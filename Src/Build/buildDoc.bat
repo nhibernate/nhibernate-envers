4e23d16bdbe6457c5621111f4fe9d8ed430dd438
Roger Kratz <roger.kratz@gmail.com>
2011-03-11 13:56:30 +0100

    more on docs

---
+++ Src/Build/buildDoc.bat
@@ -0,0 +1,8 @@
+@echo off
+SET sn="%programfiles%\Microsoft SDKs\Windows\V6.0A\Bin\sn.exe"
+set gacutil="%programfiles%\Microsoft SDKs\Windows\V6.0A\Bin\gacutil.exe"
+set msbuild="%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
+set configuration=Release
+%msbuild% default.msbuild /v:n /t:BuildPdf
+echo -------------------------------
+pause
