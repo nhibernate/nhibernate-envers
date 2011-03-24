29c238059ffcb57eb14e871e2efee4d0081d7411
Roger Kratz <roger.kratz@gmail.com>
2011-03-24 14:07:40 +0100

    minor

--- Src/NHibernate.Envers/Entities/Mapper/Relation/ToOneDelegateSessionImplementor.cs
+++ Src/NHibernate.Envers/Entities/Mapper/Relation/ToOneDelegateSessionImplementor.cs
@@ -1,24 +1,19 @@
-﻿using System;
-using NHibernate.Envers.Configuration;
+﻿using NHibernate.Envers.Configuration;
 using NHibernate.Envers.Reader;
 
 namespace NHibernate.Envers.Entities.Mapper.Relation
 {
-	/**
-	 * @author Catalina Panait, port of Envers omonyme class by Adam Warski (adam at warski dot org)
-															  * Tomasz Bech
-	 */
 	public class ToOneDelegateSessionImplementor : AbstractDelegateSessionImplementor 
 	{
 		private readonly IAuditReaderImplementor versionsReader;
 		private readonly System.Type entityClass; 
-		private readonly Object entityId;
+		private readonly object entityId;
 		private readonly long revision;
 		private readonly EntityConfiguration notVersionedEntityConfiguration;
 
 		public ToOneDelegateSessionImplementor(IAuditReaderImplementor versionsReader,
-											   System.Type entityClass, 
-											   Object entityId, long revision, 
+											   System.Type entityClass,
+											   object entityId, long revision, 
 											   AuditConfiguration verCfg) 
 			: base(versionsReader.SessionImplementor)
 		{
@@ -26,17 +21,14 @@ namespace NHibernate.Envers.Entities.Mapper.Relation
 			this.entityClass = entityClass;
 			this.entityId = entityId;
 			this.revision = revision;
-			EntitiesConfigurations entCfg = verCfg.EntCfg;
+			var entCfg = verCfg.EntCfg;
 			notVersionedEntityConfiguration = entCfg.GetNotVersionEntityConfiguration(entityClass.FullName);
 		}
 
-		protected override Object DoImmediateLoad(String entityName) 
+		protected override object DoImmediateLoad(string entityName)
 		{
-			if (notVersionedEntityConfiguration == null) 
-			{
-				return versionsReader.Find(entityClass, entityId, revision);
-			} 
-			return SessionDelegate.ImmediateLoad(entityName, entityId);
+			return notVersionedEntityConfiguration == null ? 
+				versionsReader.Find(entityClass, entityId, revision) : SessionDelegate.ImmediateLoad(entityName, entityId);
 		}
 	}
 
