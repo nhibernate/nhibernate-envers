efad989e883072c5857bb743f42f990efe496c83
Roger Kratz <roger.kratz@gmail.com>
2011-07-26 16:37:34 +0200

    Introducing ICollectionProxyTypeFactory

---
+++ Src/NHibernate.Envers/Configuration/Metadata/DefaultCollectionProxyTypeFactory.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Iesi.Collections.Generic;
+using NHibernate.Envers.Entities.Mapper;
+using NHibernate.Envers.Entities.Mapper.Relation;
+using NHibernate.Envers.Entities.Mapper.Relation.Lazy.Proxy;
+
+namespace NHibernate.Envers.Configuration.Metadata
+{
+	public class DefaultCollectionProxyTypeFactory : ICollectionProxyTypeFactory
+	{
+		public virtual IPropertyMapper Set<T>(CommonCollectionMapperData commonCollectionMapperData, MiddleComponentData elementComponentData)
+		{
+			return new SetCollectionMapper<T>(commonCollectionMapperData, 
+			                                  typeof (HashedSet<T>), 
+			                                  typeof (SetProxy<T>),
+			                                  elementComponentData);
+		}
+
+		public virtual IPropertyMapper Set(CommonCollectionMapperData commonCollectionMapperData, MiddleComponentData elementComponentData)
+		{
+			throw new NotImplementedException("Non generic set is not supported by DefaultCollectionProxyTypeFactory");
+		}
+
+		public virtual IPropertyMapper List<T>(CommonCollectionMapperData commonCollectionMapperData, MiddleComponentData elementComponentData, MiddleComponentData indexComponentData)
+		{
+			return new ListCollectionMapper<T>(commonCollectionMapperData, elementComponentData, indexComponentData);
+		}
+
+		public virtual IPropertyMapper List(CommonCollectionMapperData commonCollectionMapperData, MiddleComponentData elementComponentData, MiddleComponentData indexComponentData)
+		{
+			throw new NotImplementedException("Non generic list is not supported by DefaultCollectionProxyTypeFactory");
+		}
+
+		public virtual IPropertyMapper Map<TKey, TValue>(CommonCollectionMapperData commonCollectionMapperData, MiddleComponentData elementComponentData, MiddleComponentData indexComponentData)
+		{
+			return new MapCollectionMapper<TKey, TValue>(commonCollectionMapperData, elementComponentData, indexComponentData);
+		}
+
+		public virtual IPropertyMapper Map(CommonCollectionMapperData commonCollectionMapperData, MiddleComponentData elementComponentData, MiddleComponentData indexComponentData)
+		{
+			throw new NotImplementedException("Non generic map is not supported by DefaultCollectionProxyTypeFactory");
+		}
+
+		public virtual IPropertyMapper Bag<T>(CommonCollectionMapperData commonCollectionMapperData, MiddleComponentData elementComponentData)
+		{
+			return new BagCollectionMapper<T>(commonCollectionMapperData, elementComponentData);
+		}
+
+		public virtual IPropertyMapper Bag(CommonCollectionMapperData commonCollectionMapperData, MiddleComponentData elementComponentData)
+		{
+			throw new NotImplementedException("Non generic bag is not supported by DefaultCollectionProxyTypeFactory");
+		}
+	}
+}
---
+++ Src/NHibernate.Envers/Configuration/Metadata/ICollectionProxyTypeFactory.cs
@@ -0,0 +1,17 @@
+﻿using NHibernate.Envers.Entities.Mapper;
+using NHibernate.Envers.Entities.Mapper.Relation;
+
+namespace NHibernate.Envers.Configuration.Metadata
+{
+	public interface ICollectionProxyTypeFactory
+	{
+		IPropertyMapper Set<T>(CommonCollectionMapperData commonCollectionMapperData, MiddleComponentData elementComponentData);
+		IPropertyMapper Set(CommonCollectionMapperData commonCollectionMapperData, MiddleComponentData elementComponentData);
+		IPropertyMapper List<T>(CommonCollectionMapperData commonCollectionMapperData, MiddleComponentData elementComponentData, MiddleComponentData indexComponentData);
+		IPropertyMapper List(CommonCollectionMapperData commonCollectionMapperData, MiddleComponentData elementComponentData, MiddleComponentData indexComponentData);
+		IPropertyMapper Map<TKey, TValue>(CommonCollectionMapperData commonCollectionMapperData, MiddleComponentData elementComponentData, MiddleComponentData indexComponentData);
+		IPropertyMapper Map(CommonCollectionMapperData commonCollectionMapperData, MiddleComponentData elementComponentData, MiddleComponentData indexComponentData);
+		IPropertyMapper Bag<T>(CommonCollectionMapperData commonCollectionMapperData, MiddleComponentData elementComponentData);
+		IPropertyMapper Bag(CommonCollectionMapperData commonCollectionMapperData, MiddleComponentData elementComponentData);
+	}
+}
