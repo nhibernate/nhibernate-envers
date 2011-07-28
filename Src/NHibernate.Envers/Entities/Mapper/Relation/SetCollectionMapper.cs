019ddcd39bd1fc388a56f314f0c9b3b0aac7942d
Roger Kratz <roger.kratz@gmail.com>
2011-07-28 15:03:59 +0200

    minor

--- Src/NHibernate.Envers/Entities/Mapper/Relation/SetCollectionMapper.cs
+++ Src/NHibernate.Envers/Entities/Mapper/Relation/SetCollectionMapper.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using Iesi.Collections.Generic;
 using NHibernate.Collection;
 using NHibernate.Envers.Configuration;
 using NHibernate.Envers.Entities.Mapper.Relation.Lazy.Initializor;
