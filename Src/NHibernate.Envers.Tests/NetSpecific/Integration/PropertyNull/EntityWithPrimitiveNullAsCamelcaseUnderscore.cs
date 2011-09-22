55336fcde3685d2c640825c95ea2bc0b0bc89ac2
Roger Kratz <roger.kratz@gmail.com>
2011-09-22 17:22:51 +0200

    NHE-26 fixed

---
+++ Src/NHibernate.Envers.Tests/NetSpecific/Integration/PropertyNull/EntityWithPrimitiveNullAsCamelcaseUnderscore.cs
@@ -0,0 +1,16 @@
+﻿using NHibernate.Envers.Configuration.Attributes;
+
+namespace NHibernate.Envers.Tests.NetSpecific.Integration.PropertyNull
+{
+	[Audited]
+	public class EntityWithPrimitiveNullAsCamelcaseUnderscore
+	{
+		private int? _value;
+
+		public virtual int Id { get; set; }
+		public virtual int? Value
+		{
+			get { return _value; }
+		}
+	}
+}
