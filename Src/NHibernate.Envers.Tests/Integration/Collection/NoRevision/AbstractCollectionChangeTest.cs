4daa6624b371710d51593fde550c4f40b99d0c77
Roger Kratz <roger.kratz@gmail.com>
2011-10-03 19:53:40 +0200

    adding missing test for ConfigurationKey.RevisionOnCollectionChange

---
+++ Src/NHibernate.Envers.Tests/Integration/Collection/NoRevision/AbstractCollectionChangeTest.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using NHibernate.Envers.Configuration;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace NHibernate.Envers.Tests.Integration.Collection.NoRevision
+{
+	[TestFixture]
+	public abstract class AbstractCollectionChangeTest : TestBase
+	{
+	   protected override void AddToConfiguration(Cfg.Configuration configuration)
+		{
+			configuration.SetProperty(ConfigurationKey.RevisionOnCollectionChange, RevisionOnCollectionChange);
+		}
+
+		protected override void Initialize()
+		{
+			var n = new Name{TheName="name1"};
+			var p = new Person();
+			p.Names.Add(n);
+
+			//Rev 1
+			using (var tx = Session.BeginTransaction())
+			{
+				PersonId = (int) Session.Save(p);
+				tx.Commit();
+			}
+
+			//Rev 2
+			using (var tx = Session.BeginTransaction())
+			{
+				n.TheName = "Changed name";
+				tx.Commit();
+			}
+
+			//Rev 3
+			using (var tx = Session.BeginTransaction())
+			{
+				var n2 = new Name {TheName = "name2"};
+				p.Names.Add(n2);
+				tx.Commit();
+			}
+		}
+
+		[Test]
+		public void VerifyPersonRevisionCount() 
+		{
+			AuditReader().GetRevisions<Person>(PersonId)
+				.Should().Have.SameSequenceAs(ExpectedPersonRevisions);
+		}
+
+		private int PersonId { get; set; }
+		protected abstract string RevisionOnCollectionChange { get; }
+		protected abstract IEnumerable<long> ExpectedPersonRevisions { get; }
+	}
+}
---
+++ Src/NHibernate.Envers.Tests/Integration/Collection/NoRevision/Name.cs
@@ -0,0 +1,12 @@
+﻿using NHibernate.Envers.Configuration.Attributes;
+
+namespace NHibernate.Envers.Tests.Integration.Collection.NoRevision
+{
+	[Audited]
+	public class Name
+	{
+		public virtual int Id { get; set; }
+		public virtual string TheName { get; set; }
+		public virtual Person Person { get; set; }
+	}
+}
