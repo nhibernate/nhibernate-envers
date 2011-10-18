5e05d6053a30307e935557d6a8e8ab6393b7f59d
Roger Kratz <roger.kratz@gmail.com>
2011-10-18 15:47:36 +0200

    more on NHE-35

---
+++ Src/NHibernate.Envers.Tests/Integration/RevEntity/TrackModifiedEntities/Car.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using NHibernate.Envers.Configuration.Attributes;
+
+namespace NHibernate.Envers.Tests.Integration.RevEntity.TrackModifiedEntities
+{
+	[Audited]
+	public class Car
+	{
+		public virtual long Id { get; set; }
+		public virtual int Number { get; set; }
+		public virtual IList<Person> Owners { get; set; }
+	}
+}
