57efcad6cb19e02640c095bd94970ec1ea63cee8
Roger Kratz <roger.kratz@gmail.com>
2011-10-20 12:53:31 +0200

    fix for NHE-36

--- Src/NHibernate.Envers/Configuration/Fluent/IFluentAudit.cs
+++ Src/NHibernate.Envers/Configuration/Fluent/IFluentAudit.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq.Expressions;
+using NHibernate.Envers.Configuration.Attributes;
 
 namespace NHibernate.Envers.Configuration.Fluent
 {
@@ -32,5 +33,21 @@ namespace NHibernate.Envers.Configuration.Fluent
 		/// <param name="property"></param>
 		/// <returns></returns>
 		IFluentAudit<T> ExcludeRelationData(string property);
+
+		/// <summary>
+		/// Sets table info (name, schema and/or catalog) on a table.
+		/// This will override the default strategy for this particular table/entity.
+		/// </summary>
+		/// <param name="tableInfo">A lambda expression that will fill an <see cref="AuditTableAttribute"/></param>
+		/// <returns></returns>
+		IFluentAudit<T> SetTableInfo(Action<AuditTableAttribute> tableInfo);
+
+		/// <summary>
+		/// Sets table info (name, schema and/or catalog) on join table.
+		/// </summary>
+		/// <param name="property">A collection</param>
+		/// <param name="tableInfo">A lambda expression that will fill an <see cref="AuditJoinTableAttribute"/></param>
+		/// <returns></returns>
+		IFluentAudit<T> SetTableInfo(Expression<Func<T, object>> property, Action<AuditJoinTableAttribute> tableInfo);
 	}
 }
