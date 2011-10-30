14cae3444d7be9b260d5d9e51357113d95037e01
Roger Kratz <roger.kratz@gmail.com>
2011-10-31 00:27:57 +0100

    finishing API for querying on entityname

---
+++ Src/NHibernate.Envers.Tests/Integration/EntityNames/SingleAssociatedNotAudited/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace NHibernate.Envers.Tests.Integration.EntityNames.SingleAssociatedNotAudited
+{
+	public class Person
+	{
+		public virtual long Id { get; set; }
+		public virtual string Name { get; set; }
+		public virtual int Age { get; set; }
+	}
+}
