14f5bc3566912508175a0d93baa1339940c463c8
Roger Kratz <roger.kratz@gmail.com>
2011-10-30 23:06:44 +0100

    continue to add API for querying on entityname

---
+++ Src/NHibernate.Envers.Tests/Integration/EntityNames/OneToManyNotAudited/Car.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using NHibernate.Envers.Configuration.Attributes;
+
+namespace NHibernate.Envers.Tests.Integration.EntityNames.OneToManyNotAudited
+{
+	public class Car
+	{
+		public virtual long Id { get; set; }
+		[Audited]
+		public virtual int Number { get; set; }
+		[Audited(TargetAuditMode = RelationTargetAuditMode.NotAudited)]
+		public virtual IList<Person> Owners { get; set; }
+	}
+}
