20e132c3d7e1d4e846cf05c9264d4b647706b24f
Roger Kratz <roger.kratz@gmail.com>
2011-10-30 22:43:05 +0100

    continue to add API for querying on entityname

---
+++ Src/NHibernate.Envers.Tests/Integration/EntityNames/ManyToManyAudited/Person.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using NHibernate.Envers.Configuration.Attributes;
+
+namespace NHibernate.Envers.Tests.Integration.EntityNames.ManyToManyAudited
+{
+	[Audited]
+	public class Person
+	{
+		public virtual long Id { get; set; }
+		public virtual string Name { get; set; }
+		public virtual int Age { get; set; }
+		public virtual IList<Car> Cars { get; set; }
+	}
+}
