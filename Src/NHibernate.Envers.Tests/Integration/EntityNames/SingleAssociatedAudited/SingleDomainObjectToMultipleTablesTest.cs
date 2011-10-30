9cde65dcea57a00f58d0c4c79ac131d56cf40330
Roger Kratz <roger.kratz@gmail.com>
2011-10-31 00:27:57 +0100

    finishing API for querying on entityname

---
+++ Src/NHibernate.Envers.Tests/Integration/EntityNames/SingleAssociatedAudited/SingleDomainObjectToMultipleTablesTest.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using SharpTestsEx;
+
+namespace NHibernate.Envers.Tests.Integration.EntityNames.SingleAssociatedAudited
+{
+	[TestFixture]
+	public class SingleDomainObjectToMultipleTablesTest : TestBase
+	{
+		private long carId;
+		private long ownerId;
+		private long driverId;
+
+		protected override void Initialize()
+		{
+			using (var tx = Session.BeginTransaction())
+			{
+				var owner = new Person {Name = "Lukasz", Age = 25};
+				var driver = new Person {Name = "Kinga", Age = 24};
+				var car = new Car {Number = 1, Driver = driver, Owner = owner};
+				ownerId = (long) Session.Save("Personaje", owner);
+				driverId = (long) Session.Save("Driveraje", driver);
+				carId = (long) Session.Save(car);
+				tx.Commit();
+			}
+		}
+
+		[Test]
+		public void VerifySingleDomainObjectToMultipleTablesMapping()
+		{
+			var carVer1 = AuditReader().Find<Car>(carId, 1);
+			var ownerVer1 = AuditReader().Find<Person>("Personaje", ownerId, 1);
+			var driverVer1 = AuditReader().Find<Person>("Driveraje", driverId, 1);
+
+			carVer1.Owner.Id.Should().Be.EqualTo(ownerVer1.Id);
+			carVer1.Driver.Id.Should().Be.EqualTo(driverVer1.Id);
+
+			ownerVer1.Name.Should().Be.EqualTo("Lukasz");
+			driverVer1.Name.Should().Be.EqualTo("Kinga");
+			carVer1.Number.Should().Be.EqualTo(1);
+		}
+	}
+}
---
+++ Src/NHibernate.Envers.Tests/Integration/EntityNames/SingleAssociatedNotAudited/Car.cs
@@ -0,0 +1,13 @@
+﻿using NHibernate.Envers.Configuration.Attributes;
+
+namespace NHibernate.Envers.Tests.Integration.EntityNames.SingleAssociatedNotAudited
+{
+	public class Car
+	{
+		public virtual long Id { get; set; }
+		[Audited]
+		public virtual int Number { get; set; }
+		[Audited(TargetAuditMode = RelationTargetAuditMode.NotAudited)]
+		public virtual Person Owner { get; set; }
+	}
+}
