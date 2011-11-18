91430d328f4b48dc6cb536458fa588361c2c9fc8
Roger Kratz <roger.kratz@gmail.com>
2011-11-19 00:04:06 +0100

    tests ported

---
+++ Src/NHibernate.Envers.Tests/Integration/Interfaces/Inheritance/PropertiesAudited/ISimple.cs
@@ -0,0 +1,12 @@
+﻿using NHibernate.Envers.Configuration.Attributes;
+
+namespace NHibernate.Envers.Tests.Integration.Interfaces.Inheritance.PropertiesAudited
+{
+	public interface ISimple
+	{
+		long Id { get; set; }
+		string Data { get; set; }
+		[Audited]
+		int Number { get; set; }
+	}
+}
---
+++ Src/NHibernate.Envers.Tests/Integration/Interfaces/Inheritance/PropertiesAudited/Joined/JoinedAllAuditedTest.cs
@@ -0,0 +1,9 @@
+﻿using NUnit.Framework;
+
+namespace NHibernate.Envers.Tests.Integration.Interfaces.Inheritance.PropertiesAudited.Joined
+{
+	[TestFixture]
+	public class JoinedAllAuditedTest : AbstractAllAuditedTest
+	{
+	}
+}
