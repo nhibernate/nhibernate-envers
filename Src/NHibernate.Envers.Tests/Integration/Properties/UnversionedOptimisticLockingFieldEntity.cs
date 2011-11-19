5370747c188b430dda1b4ed5dc0be6436efac0f9
Roger Kratz <roger.kratz@gmail.com>
2011-11-19 15:26:08 +0100

    porting some tests

---
+++ Src/NHibernate.Envers.Tests/Integration/Properties/UnversionedOptimisticLockingFieldEntity.cs
@@ -0,0 +1,28 @@
+﻿using NHibernate.Envers.Configuration.Attributes;
+
+namespace NHibernate.Envers.Tests.Integration.Properties
+{
+	[Audited]
+	public class UnversionedOptimisticLockingFieldEntity
+	{
+		public virtual int Id { get; set; }
+		public virtual string Str { get; set; }
+		public virtual int OptLocking { get; set; }
+
+		public override bool Equals(object obj)
+		{
+			var that = obj as UnversionedOptimisticLockingFieldEntity;
+			if (that == null)
+				return false;
+			if (Str != null ? !Str.Equals(that.Str) : that.Str != null)
+				return false;
+			return Id == that.Id && OptLocking == that.OptLocking;
+		}
+
+		public override int GetHashCode()
+		{
+			var strHash = Str != null ? Str.GetHashCode() : 0;
+			return Id ^ strHash ^ OptLocking;
+		} 
+	}
+}
---
+++ Src/NHibernate.Envers.Tests/Integration/Properties/UnversionedOptimisticLockingFieldTest.cs
@@ -0,0 +1,61 @@
+﻿using NHibernate.Envers.Configuration;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace NHibernate.Envers.Tests.Integration.Properties
+{
+	[TestFixture]
+	public class UnversionedOptimisticLockingFieldTest : TestBase
+	{
+		private int id;
+
+		protected override void AddToConfiguration(Cfg.Configuration configuration)
+		{
+			configuration.SetProperty(ConfigurationKey.DoNotAuditOptimisticLockingField, "true");
+		}
+
+		protected override void Initialize()
+		{
+			var olfe = new UnversionedOptimisticLockingFieldEntity{Str="x"};
+			using (var tx = Session.BeginTransaction())
+			{
+				id = (int) Session.Save(olfe);
+				tx.Commit();
+			}
+			using (var tx = Session.BeginTransaction())
+			{
+				olfe.Str = "y";
+				tx.Commit();
+			}
+		}
+
+		[Test]
+		public void VerifyRevisionCounts()
+		{
+			AuditReader().GetRevisions(typeof (UnversionedOptimisticLockingFieldEntity), id)
+				.Should().Have.SameSequenceAs(1, 2);
+		}
+
+		[Test]
+		public void VerifyHistory()
+		{
+			var ver1 = new UnversionedOptimisticLockingFieldEntity {Id = id, Str = "x"};
+			var ver2 = new UnversionedOptimisticLockingFieldEntity {Id = id, Str = "y"};
+
+			AuditReader().Find<UnversionedOptimisticLockingFieldEntity>(id, 1)
+				.Should().Be.EqualTo(ver1);
+			AuditReader().Find<UnversionedOptimisticLockingFieldEntity>(id, 2)
+				.Should().Be.EqualTo(ver2);
+		}
+
+		[Test]
+		public void ShouldNotStoreVersionNumber()
+		{
+			var pc = Cfg.GetClassMapping(typeof (UnversionedOptimisticLockingFieldEntity).FullName + "_AUD");
+			foreach (var property in pc.PropertyIterator)
+			{
+				property.Name.Should().Not.Be.EqualTo("OptLocking");
+			}
+		}
+	}
+}
