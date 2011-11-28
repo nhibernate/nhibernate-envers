d2cd56472a96e1e7e8de0c68bc8de654a89dbaea
Roger Kratz <roger.kratz@gmail.com>
2011-11-28 16:53:08 +0100

    better comment

--- Src/NHibernate.Envers/Configuration/Attributes/AuditedAttribute.cs
+++ Src/NHibernate.Envers/Configuration/Attributes/AuditedAttribute.cs
@@ -26,10 +26,14 @@ namespace NHibernate.Envers.Configuration.Attributes
 		public RelationTargetAuditMode TargetAuditMode { get; set; }
 
 		/// <summary>
-		/// Set of superclasses which properties shall be audited. The behavior of listed classes
-		/// is the same as if they had <see cref="AuditedAttribute"/> annotation applied on a type level. The scope of this functionality
-		/// is limited to the context of actually mapped entity and its class hierarchy. If a parent type lists any of
-		/// its parent types using this attribute, all fields encapsulated by marked classes are implicitly audited.
+		/// Specifies the superclasses for which properties should be audited, even if the superclasses are not
+		/// annotated with <see cref="AuditedAttribute"/>. Causes all properties of the listed classes to be audited, just as if the
+		/// classes had <see cref="AuditedAttribute"/> annotation applied on the class level.
+		/// 
+		/// The scope of this functionality is limited to the class hierarchy of the annotated entity.
+		/// 
+		/// If a parent type lists any of its parent types using this attribute, all properties in the specified classes
+		/// will also be audited.
 		/// </summary>
 		public System.Type[] AuditParents { get; set; }
 	}
