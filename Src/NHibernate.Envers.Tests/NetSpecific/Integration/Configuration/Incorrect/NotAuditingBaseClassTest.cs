5dd7d78240f40a211026aff5c6202fbd28a652c6
Roger Kratz <roger.kratz@gmail.com>
2011-12-02 11:22:27 +0100

    throwing mappingexception if concrete type is audited, but base type is not

---
+++ Src/NHibernate.Envers.Tests/NetSpecific/Integration/Configuration/Incorrect/NotAuditingBaseClassTest.cs
@@ -0,0 +1,20 @@
+﻿using NHibernate.Cfg;
+using NUnit.Framework;
+
+namespace NHibernate.Envers.Tests.NetSpecific.Integration.Configuration.Incorrect
+{
+	[TestFixture]
+	public class NotAuditingBaseClassTest
+	{
+		[Test]
+		public void ShouldThrowMappingException()
+		{
+			var assembly = GetType().Assembly;
+			var assemblyName = assembly.GetName().Name;
+			var configuration = new Cfg.Configuration()
+							.Configure()
+							.AddResource(assemblyName + ".NetSpecific.Integration.Configuration.Incorrect.Mapping.hbm.xml", assembly);
+			Assert.Throws<MappingException>(() => configuration.IntegrateWithEnvers());
+		}
+	}
+}
