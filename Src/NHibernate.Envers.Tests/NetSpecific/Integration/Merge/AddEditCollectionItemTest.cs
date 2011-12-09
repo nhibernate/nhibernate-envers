ba8523ccf10205c2ece50368a549080639b39592
Roger Kratz <roger.kratz@gmail.com>
2011-12-09 17:06:23 +0100

    Adding test for NHE-44

---
+++ Src/NHibernate.Envers.Tests/NetSpecific/Integration/Merge/AddEditCollectionItemTest.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using NHibernate.Envers.Tests.Entities.OneToMany;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace NHibernate.Envers.Tests.NetSpecific.Integration.Merge
+{
+	[TestFixture]
+	public class AddEditCollectionItemTest : TestBase
+	{
+		private const int entityId = 342;
+		private const int childId = 1000;
+
+		protected override void Initialize()
+		{
+			var entity = new SetRefEdEntity { Id = entityId, Data = "data" };
+			var child = new SetRefIngEntity {Id = childId, Data = "data", Reference = entity};
+
+			//revision 1
+			using (var tx = Session.BeginTransaction())
+			{
+				Session.Save(entity);
+				tx.Commit();
+			}
+
+			//revision 2
+			using (var tx = Session.BeginTransaction())
+			{
+				Session.Save(child);
+				child.Data = "changed data";
+				tx.Commit();
+			}
+		}
+
+		[Test]
+		public void RevisionForOwned()
+		{
+			AuditReader().GetRevisions(typeof(SetRefEdEntity), entityId)
+				.Should().Have.SameSequenceAs(1);
+		}
+
+
+		[Test]
+		public void RevisionForChild()
+		{
+			AuditReader().GetRevisions(typeof(SetRefIngEntity), childId)
+				.Should().Have.SameSequenceAs(2);
+		}
+
+		protected override IEnumerable<string> Mappings
+		{
+			get { return new[] { "Entities.OneToMany.Mapping.hbm.xml" }; }
+		}
+	}
+}
