a6e14b9b8f001eb8b611e1dcaf9041b3a926e4da
Roger Kratz <roger.kratz@gmail.com>
2011-12-09 16:40:53 +0100

    minor. include param name in thrown argument*exception

--- Src/NHibernate.Envers/Tools/ArgumentsTools.cs
+++ Src/NHibernate.Envers/Tools/ArgumentsTools.cs
@@ -2,22 +2,22 @@
 
 namespace NHibernate.Envers.Tools
 {
-    public static class ArgumentsTools
-    {
-        public static void CheckNotNull(object o, string paramName)
-        {
-            if (o == null)
-            {
-                throw new ArgumentNullException(paramName + " cannot be null.");
-            }
-        }
+	public static class ArgumentsTools
+	{
+		public static void CheckNotNull(object o, string paramName)
+		{
+			if (o == null)
+			{
+				throw new ArgumentNullException(paramName, paramName + " cannot be null.");
+			}
+		}
 
-        public static void CheckPositive(long i, string paramName)
-        {
-            if (i <= 0)
-            {
-                throw new ArgumentOutOfRangeException(paramName + " has to be greater than 0.");
-            }
-        }
-    }
+		public static void CheckPositive(long i, string paramName)
+		{
+			if (i <= 0)
+			{
+				throw new ArgumentOutOfRangeException(paramName, paramName + " has to be greater than 0.");
+			}
+		}
+	}
 }
