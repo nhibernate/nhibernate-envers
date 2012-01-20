2fef68d348793d6e54952a091ae1f62f4d9d8dd4
Roger Kratz <roger.kratz@gmail.com>
2012-01-20 01:20:24 +0100

    porting some hibernate envers tests about one-to-one mapping

---
+++ Src/NHibernate.Envers.Tests/Integration/OneToOne/BiDirectional/PrimaryKeyJoinColumn/Account.cs
@@ -0,0 +1,30 @@
+﻿using NHibernate.Envers.Configuration.Attributes;
+
+namespace NHibernate.Envers.Tests.Integration.OneToOne.BiDirectional.PrimaryKeyJoinColumn
+{
+	[Audited]
+	public class Account
+	{
+		public virtual long AccountId { get; set; }
+		public virtual string Type { get; set; }
+		public virtual Person Owner { get; set; }
+
+		public override bool Equals(object obj)
+		{
+			var casted = obj as Account;
+			if (casted == null)
+				return false;
+			if (AccountId != casted.AccountId)
+				return false;
+			if (Type != null ? !Type.Equals(casted.Type) : casted.Type != null) 
+				return false;
+			return true;
+		}
+
+		public override int GetHashCode()
+		{
+			var typeResult = Type != null ? Type.GetHashCode() : 0;
+			return (int)AccountId ^ typeResult;
+		}
+	}
+}
---
+++ Src/NHibernate.Envers.Tests/Integration/OneToOne/BiDirectional/PrimaryKeyJoinColumn/AccountNotAuditedOwners.cs
@@ -0,0 +1,33 @@
+﻿using NHibernate.Envers.Configuration.Attributes;
+
+namespace NHibernate.Envers.Tests.Integration.OneToOne.BiDirectional.PrimaryKeyJoinColumn
+{
+	[Audited]
+	public class AccountNotAuditedOwners
+	{
+		public virtual long AccountId { get; set; }
+		public virtual string Type { get; set; }
+		[Audited(TargetAuditMode = RelationTargetAuditMode.NotAudited)]
+		public virtual NotAuditedNoProxyPerson Owner { get; set; }
+		[Audited(TargetAuditMode = RelationTargetAuditMode.NotAudited)]
+		public virtual NotAuditedProxyPerson CoOwner { get; set; }
+
+		public override bool Equals(object obj)
+		{
+			var casted = obj as AccountNotAuditedOwners;
+			if (casted == null)
+				return false;
+			if (AccountId != casted.AccountId)
+				return false;
+			if (Type != null ? !Type.Equals(casted.Type) : casted.Type != null)
+				return false;
+			return true;
+		}
+
+		public override int GetHashCode()
+		{
+			var typeResult = Type != null ? Type.GetHashCode() : 0;
+			return (int)AccountId ^ typeResult;
+		}
+	}
+}
