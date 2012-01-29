2849c3935f5cb31b4b536138af29068bae6b84b4
Roger Kratz <roger.kratz@gmail.com>
2012-01-30 00:06:56 +0100

    failing tests for NHE-60

---
+++ Src/NHibernate.Envers.Tests/Integration/ManyToOne/BiDirectional/ManyToOneOwning.cs
@@ -0,0 +1,26 @@
+﻿using NHibernate.Envers.Configuration.Attributes;
+
+namespace NHibernate.Envers.Tests.Integration.ManyToOne.BiDirectional
+{
+	[Audited]
+	public class ManyToOneOwning
+	{
+		public virtual long Id { get; set; }
+		public virtual string Data { get; set; }
+		public virtual OneToManyOwned References { get; set; }
+
+		public override bool Equals(object obj)
+		{
+			var casted = obj as ManyToOneOwning;
+			if (casted == null)
+				return false;
+			if (Data != null ? !Data.Equals(casted.Data) : casted.Data != null) return false;
+			return Id == casted.Id;
+		}
+
+		public override int GetHashCode()
+		{
+			return Id.GetHashCode() ^ Data.GetHashCode();
+		}
+	}
+}
---
+++ Src/NHibernate.Envers.Tests/Integration/ManyToOne/BiDirectional/OneToManyOwned.cs
@@ -0,0 +1,27 @@
+﻿using Iesi.Collections.Generic;
+using NHibernate.Envers.Configuration.Attributes;
+
+namespace NHibernate.Envers.Tests.Integration.ManyToOne.BiDirectional
+{
+	[Audited]
+	public class OneToManyOwned
+	{
+		public virtual long Id { get; set; }
+		public virtual string Data { get; set; }
+		public virtual ISet<ManyToOneOwning> Referencing { get; set; }
+
+		public override bool Equals(object obj)
+		{
+			var casted = obj as OneToManyOwned;
+			if (casted == null)
+				return false;
+			if (Data != null ? !Data.Equals(casted.Data) : casted.Data != null) return false;
+			return Id == casted.Id;
+		}
+
+		public override int GetHashCode()
+		{
+			return Id.GetHashCode() ^ Data.GetHashCode();
+		}
+	}
+}
