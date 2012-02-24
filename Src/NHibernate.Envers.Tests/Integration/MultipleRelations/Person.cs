a7ab93d807c22965c544a1d67720b39efe3c85b9
Roger Kratz <roger.kratz@gmail.com>
2012-02-24 22:44:04 +0100

    porting HHH-7073

---
+++ Src/NHibernate.Envers.Tests/Integration/MultipleRelations/Person.cs
@@ -0,0 +1,36 @@
+﻿using Iesi.Collections.Generic;
+using NHibernate.Envers.Configuration.Attributes;
+
+namespace NHibernate.Envers.Tests.Integration.MultipleRelations
+{
+	[Audited]
+	public class Person
+	{
+		public Person()
+		{
+			Addresses = new HashedSet<Address>();
+			OwnedAddresses = new HashedSet<Address>();
+		}
+
+		public virtual long Id { get; set; }
+		public virtual string Name { get; set; }
+		public virtual ISet<Address> Addresses { get; set; }
+		public virtual ISet<Address> OwnedAddresses { get; set; }
+
+		public override bool Equals(object obj)
+		{
+			var casted = obj as Person;
+			if (casted == null)
+				return false;
+			if (Name != null ? !Name.Equals(casted.Name) : casted.Name != null)
+				return false;
+			return Id == casted.Id;
+		}
+
+		public override int GetHashCode()
+		{
+			var res = Id.GetHashCode();
+			return 31 * res + (Name != null ? Name.GetHashCode() : 0);
+		}
+	}
+}
