85f23c01a28fcdb9b2f33273d33310730dc136ab
Roger Kratz <roger.kratz@gmail.com>
2012-03-07 23:19:22 +0100

    fix for NHE-65 (thanks to Tobias Wolschon)

---
+++ Src/NHibernate.Envers/Configuration/Fluent/BaseFluentAudit.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using NHibernate.Envers.Configuration.Attributes;
+
+namespace NHibernate.Envers.Configuration.Fluent
+{
+	public abstract class BaseFluentAudit : IAttributeProvider
+	{
+		protected readonly ICollection<MemberInfoAndAttribute> AttributeCollection;
+		private const BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public;
+
+		protected BaseFluentAudit()
+		{
+			AttributeCollection = new List<MemberInfoAndAttribute>();
+		}
+
+		protected void Exclude(System.Type entityType, string property)
+		{
+			var member = getMemberOrThrow(entityType, property);
+			AttributeCollection.Add(new MemberInfoAndAttribute(entityType, member, new NotAuditedAttribute()));
+		}
+
+		protected void ExcludeRelationData(System.Type entityType, string property)
+		{
+			var member = getMemberOrThrow(entityType, property);
+			var attr = new AuditedAttribute { TargetAuditMode = RelationTargetAuditMode.NotAudited };
+			AttributeCollection.Add(new MemberInfoAndAttribute(entityType, member, attr));
+		}
+
+		protected void SetTableInfo(System.Type entityType, Action<AuditTableAttribute> tableInfo)
+		{
+			var attr = new AuditTableAttribute(string.Empty);
+			tableInfo(attr);
+			AttributeCollection.Add(new MemberInfoAndAttribute(entityType, attr));
+		}
+
+		protected void SetTableInfo(System.Type entityType, string property, Action<AuditJoinTableAttribute> tableInfo)
+		{
+			var member = getMemberOrThrow(entityType, property);
+			var attr = new AuditJoinTableAttribute();
+			tableInfo(attr);
+			AttributeCollection.Add(new MemberInfoAndAttribute(entityType, member, attr));
+		}
+
+		private static MemberInfo getMemberOrThrow(System.Type entityType, string propertyName)
+		{
+			var member = entityType.GetField(propertyName, bindingFlags) ?? entityType.GetProperty(propertyName, bindingFlags) as MemberInfo;
+			if (member == null)
+			{
+				var baseType = entityType.BaseType;
+				if (baseType != null && baseType != typeof(object))
+					return getMemberOrThrow(baseType, propertyName);
+				throw new FluentException("Cannot find member " + propertyName + " on type " + entityType);
+			}
+			return member;
+		}
+
+		public IEnumerable<MemberInfoAndAttribute> Attributes(Cfg.Configuration nhConfiguration)
+		{
+			return AttributeCollection;
+		}
+	}
+}
--- Src/NHibernate.Envers/Configuration/Fluent/FluentAudit.cs
+++ Src/NHibernate.Envers/Configuration/Fluent/FluentAudit.cs
@@ -1,7 +1,5 @@
 using System;
-using System.Collections.Generic;
 using System.Linq.Expressions;
-using System.Reflection;
 using NHibernate.Envers.Configuration.Attributes;
 
 namespace NHibernate.Envers.Configuration.Fluent
@@ -11,27 +9,24 @@ namespace NHibernate.Envers.Configuration.Fluent
 	/// </summary>
 	/// <seealso cref="IFluentAudit{T}"/>
 	/// <seealso cref="LooselyTypedFluentAudit"/>
-	public class FluentAudit<T> : IFluentAudit<T>, IAttributeProvider
+	public class FluentAudit<T> : BaseFluentAudit, IFluentAudit<T>
 	{
-		private readonly ICollection<MemberInfoAndAttribute> attributes;
-		private const BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public;
 
 		public FluentAudit()
 		{
-			attributes = new List<MemberInfoAndAttribute>{new MemberInfoAndAttribute(typeof(T), new AuditedAttribute())};
+			AttributeCollection.Add(new MemberInfoAndAttribute(typeof(T), new AuditedAttribute()));
 		}
 
 		public IFluentAudit<T> Exclude(Expression<Func<T, object>> property)
 		{
 			var methodInfo = property.MethodInfo();
-			attributes.Add(new MemberInfoAndAttribute(typeof(T), methodInfo, new NotAuditedAttribute()));
+			AttributeCollection.Add(new MemberInfoAndAttribute(typeof(T), methodInfo, new NotAuditedAttribute()));
 			return this;
 		}
 
 		public IFluentAudit<T> Exclude(string property)
 		{
-			var member = getMemberOrThrow(typeof(T), property);
-			attributes.Add(new MemberInfoAndAttribute(typeof(T), member, new NotAuditedAttribute()));
+			Exclude(typeof (T), property);
 			return this;
 		}
 
@@ -39,23 +34,25 @@ namespace NHibernate.Envers.Configuration.Fluent
 		{
 			var methodInfo = property.MethodInfo();
 			var attr = new AuditedAttribute {TargetAuditMode = RelationTargetAuditMode.NotAudited};
-			attributes.Add(new MemberInfoAndAttribute(typeof(T), methodInfo, attr));
+			AttributeCollection.Add(new MemberInfoAndAttribute(typeof(T), methodInfo, attr));
 			return this;
 		}
 
 		public IFluentAudit<T> ExcludeRelationData(string property)
 		{
-			var member = getMemberOrThrow(typeof(T), property);
-			var attr = new AuditedAttribute { TargetAuditMode = RelationTargetAuditMode.NotAudited };
-			attributes.Add(new MemberInfoAndAttribute(typeof(T), member, attr));
+			ExcludeRelationData(typeof (T), property);
 			return this;
 		}
 
 		public IFluentAudit<T> SetTableInfo(Action<AuditTableAttribute> tableInfo)
 		{
-			var attr = new AuditTableAttribute(string.Empty);
-			tableInfo(attr);
-			attributes.Add(new MemberInfoAndAttribute(typeof(T), attr));
+			SetTableInfo(typeof (T), tableInfo);
+			return this;
+		}
+
+		public IFluentAudit<T> SetTableInfo(string property, Action<AuditJoinTableAttribute> tableInfo)
+		{
+			SetTableInfo(typeof(T), property, tableInfo);
 			return this;
 		}
 
@@ -64,26 +61,8 @@ namespace NHibernate.Envers.Configuration.Fluent
 			var methodInfo = property.MethodInfo();
 			var attr = new AuditJoinTableAttribute();
 			tableInfo(attr);
-			attributes.Add(new MemberInfoAndAttribute(typeof(T), methodInfo, attr));
+			AttributeCollection.Add(new MemberInfoAndAttribute(typeof(T), methodInfo, attr));
 			return this;
-		}
-
-		private static MemberInfo getMemberOrThrow(System.Type entityType, string propertyName)
-		{
-			var member = entityType.GetField(propertyName, bindingFlags) ?? entityType.GetProperty(propertyName, bindingFlags) as MemberInfo;
-			if (member == null)
-			{
-				var baseType = entityType.BaseType;
-				if (baseType != null && baseType != typeof(object))
-					return getMemberOrThrow(baseType, propertyName);
-				throw new FluentException("Cannot find member " + propertyName + " on type " + entityType);
-			}
-			return member;
-		}
-
-		public IEnumerable<MemberInfoAndAttribute> Attributes(Cfg.Configuration nhConfiguration)
-		{
-			return attributes;
 		}
 	}
 }
