3c985bad570a4d29230ca55a8b456efd15dddb17
Roger Kratz <roger.kratz@gmail.com>
2012-03-26 11:36:13 +0200

    Failing tests for NHE-66 (thanks to Patrick McNally)

---
+++ Src/NHibernate.Envers.Tests/Integration/CustomType/PrimitiveCustomTest.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using NHibernate.Envers.Tests.Entities.CustomType;
+using NUnit.Framework;
+
+namespace NHibernate.Envers.Tests.Integration.CustomType
+{
+	[TestFixture]
+	public class PrimitiveCustomTest : TestBase
+	{
+		private int pctec_id;
+
+		protected override IEnumerable<string> Mappings
+		{
+			get { return new[] { "Entities.CustomType.Mapping.hbm.xml" }; }
+		}
+
+		protected override void Initialize()
+		{
+			var pctec = new PrimitiveCustomTypeEntity();
+
+			using (var tx = Session.BeginTransaction())
+			{
+				pctec.PrimitiveType = PrimitiveImmutableType.Get(20);
+				Session.Save(pctec);
+				tx.Commit();
+			}
+			using (var tx = Session.BeginTransaction())
+			{
+				pctec.PrimitiveType = PrimitiveImmutableType.Get(21);
+				tx.Commit();
+			}
+			pctec_id = pctec.Id;
+		}
+
+		[Test]
+		public void VerifyRevisionCount()
+		{
+			CollectionAssert.AreEquivalent(new[] { 1, 2 }, AuditReader().GetRevisions(typeof(PrimitiveCustomTypeEntity), pctec_id));
+		}
+
+		[Test]
+		public void VerifyHistoryOfPctec()
+		{
+			var rev1 = AuditReader().Find<PrimitiveCustomTypeEntity>(pctec_id, 1);
+			var rev2 = AuditReader().Find<PrimitiveCustomTypeEntity>(pctec_id, 2);
+
+			Assert.AreEqual('u', rev1.PrimitiveType.CharValue);
+			Assert.AreEqual('v', rev2.PrimitiveType.CharValue);
+		}
+	}
+}
