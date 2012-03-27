d609e28769d607aca58734fb2e5d291e566be4bc
Roger Kratz <roger.kratz@gmail.com>
2012-03-27 23:28:31 +0200

    Adding tests for NHE-67 with three different configuration options

---
+++ Src/NHibernate.Envers.Tests/NetSpecific/Integration/Configuration/SetPropertyTest.cs
@@ -0,0 +1,50 @@
+﻿using NHibernate.Cfg;
+using NHibernate.Envers.Configuration;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace NHibernate.Envers.Tests.NetSpecific.Integration.Configuration
+{
+	[TestFixture]
+	public class SetPropertyTest
+	{
+		[Test]
+		public void CanBeSetNhCoreWay()
+		{
+			var nhConfiguration = new Cfg.Configuration();
+			nhConfiguration.SetProperty("nhibernate.envers.store_data_at_delete", "true");
+			nhConfiguration.IntegrateWithEnvers();
+			AuditConfiguration.GetFor(nhConfiguration).GlobalCfg.StoreDataAtDelete
+				.Should().Be.True();
+		}
+
+		[Test]
+		public void CanBeSetByConfigurationKey()
+		{
+			var nhConfiguration = new Cfg.Configuration();
+			nhConfiguration.SetEnversProperty(ConfigurationKey.StoreDataAtDelete, true);
+			nhConfiguration.IntegrateWithEnvers();
+			AuditConfiguration.GetFor(nhConfiguration).GlobalCfg.StoreDataAtDelete
+				.Should().Be.True();
+		}
+
+		[Test]
+		public void CanBeSetByConfigurationKeyMethod()
+		{
+			var nhConfiguration = new Cfg.Configuration();
+			ConfigurationKey.StoreDataAtDelete.SetUserValue(nhConfiguration, true);
+			nhConfiguration.IntegrateWithEnvers();
+			AuditConfiguration.GetFor(nhConfiguration).GlobalCfg.StoreDataAtDelete
+				.Should().Be.True();
+		}
+
+		[Test]
+		public void ShouldUseDefaultValue()
+		{
+			var nhConfiguration = new Cfg.Configuration();
+			nhConfiguration.IntegrateWithEnvers();
+			AuditConfiguration.GetFor(nhConfiguration).GlobalCfg.StoreDataAtDelete
+				.Should().Be.False();
+		}
+	}
+}
