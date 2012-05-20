015c1bd4c1a89f92e8bf535f70f94792bc92cdcd
Roger Kratz <roger.kratz@gmail.com>
2012-05-20 23:38:34 +0200

    adding new testbase

---
+++ Src/NHibernate.Envers.Tests/TestBase.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace NHibernate.Envers.Tests
+{
+	[TestFixture("NHibernate.Envers.Strategy.DefaultAuditStrategy, NHibernate.Envers")]
+	[TestFixture("NHibernate.Envers.Strategy.ValidityAuditStrategy, NHibernate.Envers")]
+	public abstract class TestBase : OneStrategyTestBase
+	{
+		protected TestBase(string strategyType) : base(strategyType)
+		{
+		}
+	}
+}
