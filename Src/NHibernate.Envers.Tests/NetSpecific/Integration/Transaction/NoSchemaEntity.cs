c9e7157149c5229519514a5de2d9cb159afa4f03
Roger Kratz <roger.kratz@gmail.com>
2012-06-03 23:02:03 +0200

    NHE-78 fixing yet another test (oracle)

---
+++ Src/NHibernate.Envers.Tests/NetSpecific/Integration/Transaction/NoSchemaEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using NHibernate.Envers.Configuration.Attributes;
+
+namespace NHibernate.Envers.Tests.NetSpecific.Integration.Transaction
+{
+	[Audited]
+	public class NoSchemaEntity
+	{
+		public virtual Guid Id { get; set; }
+	}
+}
