a7f041ead36b830857044f889428081d3c2e1bb3
Roger Kratz <roger.kratz@gmail.com>
2012-08-09 14:29:27 +0200

    porting a Java Envers test for emb id

---
+++ Src/NHibernate.Envers.Tests/Entities/Ids/CompositeDateIdTestEntity.cs
@@ -0,0 +1,31 @@
+﻿using NHibernate.Envers.Configuration.Attributes;
+
+namespace NHibernate.Envers.Tests.Entities.Ids
+{
+	[Audited]
+	public class CompositeDateIdTestEntity
+	{
+		public virtual DateEmbId Id { get; set; }
+		public virtual string Str1 { get; set; }
+
+		public override bool Equals(object obj)
+		{
+			var casted = obj as CompositeDateIdTestEntity;
+			if (casted == null)
+				return false;
+			if (Id != null ? !Id.Equals(casted.Id) : casted.Id != null) 
+				return false;
+			if (Str1 != null ? !Str1.Equals(casted.Str1) : casted.Str1 != null) 
+				return false;
+
+			return true;
+		}
+
+		public override int GetHashCode()
+		{
+			var result = Id != null ? Id.GetHashCode() : 0;
+			result = 31 * result + (Str1 != null ? Str1.GetHashCode() : 0);
+			return result;
+		}
+	}
+}
---
+++ Src/NHibernate.Envers.Tests/Integration/Ids/CompositeDateIdTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using NHibernate.Envers.Tests.Entities.Ids;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace NHibernate.Envers.Tests.Integration.Ids
+{
+	public class CompositeDateIdTest : TestBase
+	{
+		private DateEmbId id;
+
+		public CompositeDateIdTest(string strategyType) : base(strategyType)
+		{
+		}
+
+		protected override void Initialize()
+		{
+			id = new DateEmbId{X = new DateTime(1900,1,1), Y=new DateTime(2000,1,1)};
+			var dite = new CompositeDateIdTestEntity {Id = id, Str1 = "x"};
+			using (var tx = Session.BeginTransaction())
+			{
+				Session.Save(dite);
+				tx.Commit();
+			}
+			using (var tx = Session.BeginTransaction())
+			{
+				dite.Str1 = "y";
+				tx.Commit();
+			}
+		}
+
+
+		[Test]
+		public void VerifyRevisionCount()
+		{
+			AuditReader().GetRevisions(typeof (CompositeDateIdTestEntity), id)
+				.Should().Have.SameSequenceAs(1, 2);
+		}
+
+		[Test]
+		public void VerifyHistory()
+		{
+			AuditReader().Find<CompositeDateIdTestEntity>(id, 1).Str1
+				.Should().Be.EqualTo("x");
+			AuditReader().Find<CompositeDateIdTestEntity>(id, 2).Str1
+				.Should().Be.EqualTo("y");
+		}
+
+		protected override IEnumerable<string> Mappings
+		{
+			get { return new[] { "Entities.Ids.Mapping.hbm.xml" }; }
+		}
+
+	}
+}
