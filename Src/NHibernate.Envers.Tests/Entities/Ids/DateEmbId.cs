eae685dbb156066f3c2b3bd7870cfe51f77943c6
Roger Kratz <roger.kratz@gmail.com>
2012-08-09 14:29:27 +0200

    porting a Java Envers test for emb id

---
+++ Src/NHibernate.Envers.Tests/Entities/Ids/DateEmbId.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NHibernate.Envers.Tests.Entities.Ids
+{
+	public class DateEmbId
+	{
+		public virtual DateTime X { get; set; }
+		public virtual DateTime Y { get; set; }
+
+		public override bool Equals(object obj)
+		{
+			var casted = obj as DateEmbId;
+			if (casted == null)
+				return false;
+			return casted.X == X && casted.Y == Y;
+		}
+
+		public override int GetHashCode()
+		{
+			return X.GetHashCode() ^ Y.GetHashCode();
+		}
+	}
+}
