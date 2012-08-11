43f39101fc1a8a5b8ce168fa1f19c6a4528b34f1
Roger Kratz <roger.kratz@gmail.com>
2012-08-11 13:47:34 +0200

    porting another Java Envers test for emb id

---
+++ Src/NHibernate.Envers.Tests/Entities/Ids/ManyToOneNotAuditedEmbId.cs
@@ -0,0 +1,23 @@
+﻿namespace NHibernate.Envers.Tests.Entities.Ids
+{
+	public class ManyToOneNotAuditedEmbId
+	{
+		public virtual UnversionedStrTestEntity Id { get; set; }
+
+		public override bool Equals(object obj)
+		{
+			var casted = obj as ManyToOneNotAuditedEmbId;
+			if (casted == null)
+				return false;
+			if (Id != null ? !Id.Equals(casted.Id) : casted.Id != null) 
+				return false;
+
+			return true;
+		}
+
+		public override int GetHashCode()
+		{
+			return Id != null ? Id.GetHashCode() : 0;
+		}
+	}
+}
