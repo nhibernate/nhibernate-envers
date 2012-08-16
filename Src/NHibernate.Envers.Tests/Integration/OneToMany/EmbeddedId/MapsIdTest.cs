85be17201b86a98d11b2642f3aacd18981b6dc9c
Roger Kratz <roger.kratz@gmail.com>
2012-08-16 18:00:21 +0200

    Ignoring tests for NHE-86 for now

--- Src/NHibernate.Envers.Tests/Integration/OneToMany/EmbeddedId/MapsIdTest.cs
+++ Src/NHibernate.Envers.Tests/Integration/OneToMany/EmbeddedId/MapsIdTest.cs
@@ -3,6 +3,7 @@ using SharpTestsEx;
 
 namespace NHibernate.Envers.Tests.Integration.OneToMany.EmbeddedId
 {
+	[Ignore("NHE86 - Not yet fixed")]
 	public class MapsIdTest : TestBase
 	{
 		private PersonTuple tuple1Ver1;
