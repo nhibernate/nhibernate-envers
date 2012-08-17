21d1e386e37676c783cdd81913378c4eda29629e
Roger Kratz <roger.kratz@gmail.com>
2012-08-17 15:08:52 +0200

    added test for unidirectional one-to-many with not-null key
    Envers shouldn't really create an extra middle table for this kind of
    collection. However - it's a quite big breaking change, so I leave it as is
    for now

---
+++ Src/NHibernate.Envers.Tests/NetSpecific/Integration/OneToMany/Child.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NHibernate.Envers.Configuration.Attributes;
+
+namespace NHibernate.Envers.Tests.NetSpecific.Integration.OneToMany
+{
+	[Audited]
+	public class Child
+	{
+		public virtual Guid Id { get; set; }
+		public virtual string Str { get; set; }
+
+		public override bool Equals(object obj)
+		{
+			if (ReferenceEquals(null, obj)) return false;
+			if (ReferenceEquals(this, obj)) return true;
+			if (obj.GetType() != typeof (Child)) return false;
+			return Equals((Child) obj);
+		}
+
+		public virtual bool Equals(Child other)
+		{
+			if (ReferenceEquals(null, other)) return false;
+			if (ReferenceEquals(this, other)) return true;
+			return other.Id.Equals(Id) && Equals(other.Str, Str);
+		}
+
+		public override int GetHashCode()
+		{
+			unchecked
+			{
+				return (Id.GetHashCode()*397) ^ (Str != null ? Str.GetHashCode() : 0);
+			}
+		}
+	}
+}
