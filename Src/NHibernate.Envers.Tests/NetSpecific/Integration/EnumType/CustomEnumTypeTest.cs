d72ac1ac7f9fa7bb55567a0dbb0cad3daa987828
Roger Kratz <roger.kratz@gmail.com>
2012-09-21 18:05:43 +0200

    fix for NHE-88

---
+++ Src/NHibernate.Envers.Tests/NetSpecific/Integration/EnumType/CustomEnumTypeTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using SharpTestsEx;
+
+namespace NHibernate.Envers.Tests.NetSpecific.Integration.EnumType
+{
+	public class CustomEnumTypeTest : TestBase
+	{
+		private int id;
+
+		public CustomEnumTypeTest(string strategyType) : base(strategyType)
+		{
+		}
+
+		protected override void Initialize()
+		{
+			var entity = new Entity{EntityEnum = EntityEnum.TypeB};
+			using (var tx = Session.BeginTransaction())
+			{
+				id = (int) Session.Save(entity);
+				tx.Commit();
+			}
+		}
+
+		[Test]
+		public void VerifyRevisionCount()
+		{
+			AuditReader().GetRevisions(typeof(Entity), id)
+				.Should().Have.SameSequenceAs(1);
+		}
+
+		[Test]
+		public void VerifyHistoryOfEntity()
+		{
+			AuditReader().Find<Entity>(id, 1).EntityEnum.Should().Be.EqualTo(EntityEnum.TypeB);
+		}
+	}
+}
