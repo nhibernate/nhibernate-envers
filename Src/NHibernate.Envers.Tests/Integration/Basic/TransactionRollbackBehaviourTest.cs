8aea23fb1f645f5365d1f96fa2625f575c614221
Roger Kratz <roger.kratz@gmail.com>
2012-10-11 17:33:28 +0200

    NHE-93 fixed

---
+++ Src/NHibernate.Envers.Tests/Integration/Basic/TransactionRollbackBehaviourTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NHibernate.Envers.Tests.Entities;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace NHibernate.Envers.Tests.Integration.Basic
+{
+	public class TransactionRollbackBehaviourTest : TestBase
+	{
+		private int rollbackId;
+		private int committedId;
+
+		public TransactionRollbackBehaviourTest(string strategyType) : base(strategyType)
+		{
+		}
+
+		protected override void Initialize()
+		{
+			using (var tx = Session.BeginTransaction())
+			{
+				var iteRollback = new IntTestEntity {Number = 30};
+				rollbackId = (int) Session.Save(iteRollback);
+				tx.Rollback();
+			}
+			using (var tx = Session.BeginTransaction())
+			{
+				var iteCommitted = new IntTestEntity {Number = 50};
+				committedId = (int) Session.Save(iteCommitted);
+				tx.Commit();
+			}
+		}
+
+		[Test]
+		public void VerifyAuditRecordsRollback()
+		{
+			AuditReader().GetRevisions(typeof (IntTestEntity), committedId).Count().Should().Be.EqualTo(1);
+			AuditReader().GetRevisions(typeof (IntTestEntity), rollbackId).Should().Be.Empty();
+		}
+
+		protected override IEnumerable<string> Mappings
+		{
+			get { return new[] {"Entities.Mapping.hbm.xml"}; }
+		}
+	}
+}
