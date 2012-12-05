1cee620fab481c89c72c3cbe5f270e80fb40e7f7
Roger Kratz <roger.kratz@gmail.com>
2012-12-05 14:41:27 +0100

    now release scripts sets nuget version and file version

--- Src/Build/buildRelease.bat
+++ Src/Build/buildRelease.bat
@@ -1,8 +1,38 @@
 @echo off
-SET sn="%programfiles%\Microsoft SDKs\Windows\V6.0A\Bin\sn.exe"
-set gacutil="%programfiles%\Microsoft SDKs\Windows\V6.0A\Bin\gacutil.exe"
+SET SRCDIR=%~dp0
+set SRCDIR=%SRCDIR:~0,-7%
+set nugetfolder="%SRCDIR%\.nuget"
+set packageFolder="%SRCDIR%\packages"
 set msbuild="%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
 set configuration=Release
-%msbuild% default.msbuild /v:n /t:BuildRelease
+
+echo Before creating a release, remember to...
+echo * Update release notes
+echo * Update version number in docs
+echo.
+
+echo Installing msbuildtasks to %PackageFolder%. Please wait...
+%nugetFolder%\NuGet install MsBuildTasks -o %PackageFolder%
+
+echo.
+set /p Version=Please enter version number, eg 1.2.0.0: 
+set /p NugetVersion=Please enter nuget version number, eg 1.2: 
+
+%msbuild% default.msbuild /v:q /t:BuildRelease
+
+hg revert -C %SRCDIR%\NHibernate.Envers\Properties\AssemblyInfo.cs
+
 echo -------------------------------
+echo.
+echo Created a new nuget package with version %nugetversion% to output folder.
+echo Assembly versions set to %Version%.
+echo Created html docs to output folder
+echo.
+echo Remember to...
+echo * Tag current changeset with version %nugetversion%
+echo * Push changes (tag) to server repo
+echo * Push nuget package to nuget server (and symbol server)
+echo * Push new docs to docs repo
+
+echo.
 pause
