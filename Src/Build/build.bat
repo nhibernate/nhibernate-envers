c414fa7a6244626b3569f7d9248f88afd210026a
Roger Kratz <roger.kratz@gmail.com>
2012-12-05 13:49:09 +0100

    continue to clean up release script

--- Src/Build/build.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-SET sn="%programfiles%\Microsoft SDKs\Windows\V6.0A\Bin\sn.exe"
-set gacutil="%programfiles%\Microsoft SDKs\Windows\V6.0A\Bin\gacutil.exe"
-set msbuild="%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
-set configuration=Debug
-%msbuild% default.msbuild /v:n /t:Build
-echo -------------------------------
-pause
--- Src/Build/buildRelease.bat
+++ Src/Build/buildRelease.bat
@@ -3,6 +3,6 @@ SET sn="%programfiles%\Microsoft SDKs\Windows\V6.0A\Bin\sn.exe"
 set gacutil="%programfiles%\Microsoft SDKs\Windows\V6.0A\Bin\gacutil.exe"
 set msbuild="%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
 set configuration=Release
-%msbuild% default.msbuild /v:n /t:Build;nuget
+%msbuild% default.msbuild /v:n /t:BuildRelease
 echo -------------------------------
 pause
