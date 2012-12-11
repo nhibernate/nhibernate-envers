9f1848fff1c91586f53a2f8885821da212c93b0c
Roger Kratz <roger.kratz@gmail.com>
2012-12-11 17:56:28 +0100

    can now build docs before building release (was missing nuget package)

--- Src/Build/buildDoc.bat
+++ Src/Build/buildDoc.bat
@@ -1,8 +1,14 @@
 @echo off
-SET sn="%programfiles%\Microsoft SDKs\Windows\V6.0A\Bin\sn.exe"
-set gacutil="%programfiles%\Microsoft SDKs\Windows\V6.0A\Bin\gacutil.exe"
+SET SRCDIR=%~dp0
+set SRCDIR=%SRCDIR:~0,-7%
+set nugetfolder="%SRCDIR%\.nuget"
+set packageFolder="%SRCDIR%\packages"
 set msbuild="%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
 set configuration=Release
+
+echo Installing msbuildtasks to %PackageFolder%. Please wait...
+%nugetFolder%\NuGet install MsBuildTasks -o %PackageFolder%
+
 %msbuild% default.msbuild /v:n /t:BuildDocs
 echo -------------------------------
 pause
