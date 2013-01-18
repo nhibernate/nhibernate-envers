2aa0b33abdecca3fd616ed4fc481af5b508c8ca1
Roger Kratz <roger.kratz@gmail.com>
2013-01-18 15:34:40 +0100

    Added a test for NHE-103, verifying join table names are the same as before.

---
+++ Src/NHibernate.Envers.Tests/NetSpecific/Integration/Naming/Default/DetachedOneToManyTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace NHibernate.Envers.Tests.NetSpecific.Integration.Naming.Default
+{
+	public class DetachedOneToManyTest : TestBase
+	{
+		public DetachedOneToManyTest(string strategyType) : base(strategyType)
+		{
+		}
+
+		protected override void Initialize()
+		{
+		}
+
+		[Test]
+		public void VerifyJoinTableName()
+		{
+			const string auditEntityName = "WikiPage_WikiImage_AUD";
+			Cfg.GetClassMapping(auditEntityName).Table.Name.Should().Be.EqualTo("WikiPage_WikiImage_AUD");
+		}
+
+		protected override IEnumerable<string> Mappings
+		{
+			get
+			{
+				return new[] { "Integration.HashCode.Mapping.hbm.xml" };
+			}
+		}
+	}
+}
