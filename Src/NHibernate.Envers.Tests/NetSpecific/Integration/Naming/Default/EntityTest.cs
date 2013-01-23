5701ccc63e0287c5df2c769c5df1febad03d4971
Roger Kratz <roger.kratz@gmail.com>
2013-01-23 14:58:19 +0100

    Added more tests for NHE-103, veryfing table names same as before.

---
+++ Src/NHibernate.Envers.Tests/NetSpecific/Integration/Naming/Default/EntityTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace NHibernate.Envers.Tests.NetSpecific.Integration.Naming.Default
+{
+	public class EntityTest : TestBase
+	{
+		public EntityTest(string strategyType) : base(strategyType)
+		{
+		}
+
+		protected override void Initialize()
+		{
+		}
+
+		[Test]
+		public void VerifyTableName()
+		{
+			const string auditEntityName = "NHibernate.Envers.Tests.Entities.IntTestEntity_AUD";
+			Cfg.GetClassMapping(auditEntityName).Table.Name.Should().Be.EqualTo("IntTestEntity_AUD");
+		}
+
+		protected override IEnumerable<string> Mappings
+		{
+			get
+			{
+				return new[] { "Entities.Mapping.hbm.xml" };
+			}
+		}
+	}
+}
---
+++ Src/NHibernate.Envers.Tests/NetSpecific/Integration/Naming/Default/JoinTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace NHibernate.Envers.Tests.NetSpecific.Integration.Naming.Default
+{
+	public class JoinTest : TestBase
+	{
+		public JoinTest(string strategyType) : base(strategyType)
+		{
+		}
+
+		protected override void Initialize()
+		{
+		}
+
+		[Test]
+		public void VerifyTableName()
+		{
+			const string auditEntityName = "NHibernate.Envers.Tests.Integration.Join.JoinTestEntity_AUD";
+			Cfg.GetClassMapping(auditEntityName).JoinIterator.First().Table.Name.Should().Be.EqualTo("Secondary_AUD");
+		}
+
+		protected override IEnumerable<string> Mappings
+		{
+			get
+			{
+				return new[] { "Integration.Join.Mapping.hbm.xml" };
+			}
+		}
+	}
+}
