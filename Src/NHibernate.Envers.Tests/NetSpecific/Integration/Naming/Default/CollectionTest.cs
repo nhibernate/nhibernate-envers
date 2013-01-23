63758e919c9ab0b887d7de43e98ac61dbc80b585
Roger Kratz <roger.kratz@gmail.com>
2013-01-23 14:58:19 +0100

    Added more tests for NHE-103, veryfing table names same as before.

---
+++ Src/NHibernate.Envers.Tests/NetSpecific/Integration/Naming/Default/CollectionTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace NHibernate.Envers.Tests.NetSpecific.Integration.Naming.Default
+{
+	public class CollectionTest : TestBase
+	{
+		public CollectionTest(string strategyType) : base(strategyType)
+		{
+		}
+
+		protected override void Initialize()
+		{
+		}
+
+		[Test]
+		public void VerifyTableName()
+		{
+			const string auditEntityName = "Enums1_AUD";
+			Cfg.GetClassMapping(auditEntityName).Table.Name.Should().Be.EqualTo("Enums1_AUD");
+		}
+
+		protected override IEnumerable<string> Mappings
+		{
+			get { return new[] { "Entities.Collection.Mapping.hbm.xml" }; }
+		}
+	}
+}
