43c87c8128b0ea4f3cf010354a29596f0898083b
Roger Kratz <roger.kratz@gmail.com>
2013-02-11 17:05:58 +0100

    some clean up

--- Src/NHibernate.Envers/IEntityFactory.cs
+++ Src/NHibernate.Envers/IEntityFactory.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace NHibernate.Envers
+﻿namespace NHibernate.Envers
 {
 	/// <summary>
 	/// Contract for instantiation of an entity
