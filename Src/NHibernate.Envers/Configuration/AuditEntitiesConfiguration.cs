78322f8ef95f6fc9b52371b33bdc82e1ae9d921f
Roger Kratz <roger.kratz@gmail.com>
2013-03-01 17:22:13 +0100

    clean up

--- Src/NHibernate.Envers/Configuration/AuditEntitiesConfiguration.cs
+++ Src/NHibernate.Envers/Configuration/AuditEntitiesConfiguration.cs
@@ -35,7 +35,6 @@ namespace NHibernate.Envers.Configuration
 			_revisionPropBasePath = OriginalIdPropName + "." + RevisionFieldName + ".";
 			_namingStrategy = ConfigurationKey.TableNameStrategy.ToInstance<IEnversNamingStrategy>(properties);
 			_namingStrategy.Initialize(_auditTablePrefix, _auditTableSuffix);
-			RevisionTypePropType = "byte";
 		}
 
 
@@ -47,7 +46,6 @@ namespace NHibernate.Envers.Configuration
 		public bool IsRevisionEndTimestampEnabled { get; private set; }
 		public string RevisionEndTimestampFieldName { get; private set; }
 		public string RevisionInfoEntityAssemblyQualifiedName { get; private set; }
-		public string RevisionTypePropType { get; private set; }
 
 		/// <summary>
 		/// Returns the class name without the assembly name. Used for generating querries
