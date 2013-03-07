b47863023a172ff4d606f737574fdf9655ed510f
crazyRaisy <stefan.reissmann@conceptsoftware.de>
2013-03-07 16:43:00 +0100

    added a test to ensure that an the IRevisionEntityInfo can used covariant

---
+++ Src/NHibernate.Envers.Tests/Integration/Interfaces/CovariantInterfacesTests.cs
@@ -0,0 +1,15 @@
+﻿using NHibernate.Envers.Query;
+using NHibernate.Envers.Query.Impl;
+
+namespace NHibernate.Envers.Tests.Integration.Interfaces
+{
+    class CovariantInterfacesTests
+    {
+        private IRevisionEntityInfo<object, object> This_test_will_only_compile_if_the_typeparameter_of_the_IRevisionEntityInfo_interface_are_defined_as_covariant()
+        {
+            return new RevisionEntityInfo<CovariantToObject, CovariantToObject>(new CovariantToObject(), new CovariantToObject(), RevisionType.Added);
+        }
+
+        class CovariantToObject {}
+    }
+}
