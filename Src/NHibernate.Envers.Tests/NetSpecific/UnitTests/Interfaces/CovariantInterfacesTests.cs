475905ce45cfedb94fa7f301d201c93a6c52f35b
Roger Kratz <roger.kratz@gmail.com>
2013-03-07 17:37:36 +0100

    tabified

--- Src/NHibernate.Envers.Tests/NetSpecific/UnitTests/Interfaces/CovariantInterfacesTests.cs
+++ Src/NHibernate.Envers.Tests/NetSpecific/UnitTests/Interfaces/CovariantInterfacesTests.cs
@@ -3,13 +3,13 @@ using NHibernate.Envers.Query.Impl;
 
 namespace NHibernate.Envers.Tests.NetSpecific.UnitTests.Interfaces
 {
-    class CovariantInterfacesTests
-    {
-        private IRevisionEntityInfo<object, object> This_test_will_only_compile_if_the_typeparameter_of_the_IRevisionEntityInfo_interface_are_defined_as_covariant()
-        {
-            return new RevisionEntityInfo<CovariantToObject, CovariantToObject>(new CovariantToObject(), new CovariantToObject(), RevisionType.Added);
-        }
+	class CovariantInterfacesTests
+	{
+		private IRevisionEntityInfo<object, object> This_test_will_only_compile_if_the_typeparameter_of_the_IRevisionEntityInfo_interface_are_defined_as_covariant()
+		{
+			return new RevisionEntityInfo<CovariantToObject, CovariantToObject>(new CovariantToObject(), new CovariantToObject(), RevisionType.Added);
+		}
 
-        class CovariantToObject {}
-    }
+		class CovariantToObject { }
+	}
 }
