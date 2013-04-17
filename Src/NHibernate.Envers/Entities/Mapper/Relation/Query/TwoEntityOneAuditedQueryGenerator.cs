02b4da9a0092b1a057a6b82dfdedf51e08146d68
Roger Kratz <roger.kratz@gmail.com>
2013-04-17 23:49:03 +0200

    minor

--- Src/NHibernate.Envers/Entities/Mapper/Relation/Query/TwoEntityOneAuditedQueryGenerator.cs
+++ Src/NHibernate.Envers/Entities/Mapper/Relation/Query/TwoEntityOneAuditedQueryGenerator.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using NHibernate.Envers.Configuration;
 using NHibernate.Envers.Strategy;
 using NHibernate.Envers.Tools.Query;
