28e78bbf79c74186c91106934c7c560405a1d329
Roger Kratz <roger.kratz@gmail.com>
2013-04-22 13:17:35 +0200

    NHE-122 Use UTC for revision timestamp

---
+++ Src/NHibernate.Envers.Tests/NetSpecific/Integration/RevInfo/Time/DateTimeTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NHibernate.Envers.Tests.Entities;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace NHibernate.Envers.Tests.NetSpecific.Integration.RevInfo.Time
+{
+	public class DateTimeTest : TestBase
+	{
+		public DateTimeTest(string strategyType) : base(strategyType)
+		{
+		}
+
+		protected override void Initialize()
+		{
+			var entity = new StrTestEntity {Str = "data"};
+			using (var tx = Session.BeginTransaction())
+			{
+				Session.Save(entity);
+				tx.Commit();
+			}
+		}
+
+		[Test]
+		public void ShouldUseUtcDateForRevisionDate()
+		{
+			var utcNow = DateTime.UtcNow;
+			var res = AuditReader().CreateQuery().ForHistoryOf<StrTestEntity, DefaultRevisionEntity>().Results().First();
+			res.RevisionEntity.RevisionDate.Should().Be.IncludedIn(utcNow.AddMinutes(-1), utcNow.AddMinutes(1));
+		}
+
+
+		protected override IEnumerable<string> Mappings
+		{
+			get
+			{
+				return new[]{"Entities.Mapping.hbm.xml"};
+			}
+		}
+	}
+}
---
+++ Src/NHibernate.Envers.Tests/NetSpecific/Integration/RevInfo/Time/LongTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NHibernate.Envers.Tests.Entities;
+using NHibernate.Envers.Tests.Entities.RevEntity;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace NHibernate.Envers.Tests.NetSpecific.Integration.RevInfo.Time
+{
+	public class LongTest : TestBase
+	{
+		public LongTest(string strategyType) : base(strategyType)
+		{
+		}
+
+		protected override void Initialize()
+		{
+			var entity = new StrTestEntity { Str = "data" };
+			using (var tx = Session.BeginTransaction())
+			{
+				Session.Save(entity);
+				tx.Commit();
+			}
+		}
+
+		[Test]
+		public void ShouldUseUtcDateForRevisionDate()
+		{
+			var utcNow = DateTime.UtcNow;
+			var res = AuditReader().CreateQuery().ForHistoryOf<StrTestEntity, CustomDataRevEntity>().Results().First();
+			res.RevisionEntity.CustomTimestamp.Should().Be.IncludedIn(utcNow.AddMinutes(-1).Ticks, utcNow.AddMinutes(1).Ticks);
+		}
+
+		protected override IEnumerable<string> Mappings
+		{
+			get
+			{
+				return new[] { "Entities.Mapping.hbm.xml", "Entities.RevEntity.CustomDataRevEntity.hbm.xml" };
+			}
+		} 
+	}
+}
