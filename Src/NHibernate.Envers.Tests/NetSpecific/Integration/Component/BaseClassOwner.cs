4439d91d6cc7876bb94e32cd92cf1ebddcad09db
Tobias Wolschon <t.wolschon@euroimmun.de>
2013-06-20 15:12:17 +0200

    Added failing Unit tests for derived components

---
+++ Src/NHibernate.Envers.Tests/NetSpecific/Integration/Component/BaseClassOwner.cs
@@ -0,0 +1,11 @@
+﻿using NHibernate.Envers.Configuration.Attributes;
+
+namespace NHibernate.Envers.Tests.NetSpecific.Integration.Component
+{
+    [Audited]
+    class BaseClassOwner
+    {
+        public virtual int Id { get; set; }
+        public virtual BaseClassComponent Component { get; set; }
+    }
+}
---
+++ Src/NHibernate.Envers.Tests/NetSpecific/Integration/Component/DerivedClassComponent.cs
@@ -0,0 +1,9 @@
+﻿namespace NHibernate.Envers.Tests.NetSpecific.Integration.Component
+{
+    /// <summary>
+    /// Derives from BaseClassComponent, so it includes Data1.
+    /// </summary>
+    class DerivedClassComponent : BaseClassComponent
+    {
+    }
+}
