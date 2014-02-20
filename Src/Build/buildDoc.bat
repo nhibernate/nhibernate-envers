60c206f33e25d818c76c5fae52ce43bbd6de80c0
Roger Kratz <roger.kratz@gmail.com>
2014-02-20 10:01:37 +0100

    using specific msbuildtask version for building docs

--- Src/Build/buildDoc.bat
+++ Src/Build/buildDoc.bat
@@ -5,9 +5,10 @@ set nugetfolder="%SRCDIR%\.nuget"
 set packageFolder="%SRCDIR%\packages"
 set msbuild="%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
 set configuration=Release
+set msbuildtasksVersion=1.4.0.65
 
 echo Installing msbuildtasks to %PackageFolder%. Please wait...
-%nugetFolder%\NuGet install MsBuildTasks -o %PackageFolder%
+%nugetFolder%\NuGet install MsBuildTasks -o %PackageFolder% -Version %msbuildtasksVersion%
 
 %msbuild% default.msbuild /v:n /t:BuildDocs
 echo -------------------------------
