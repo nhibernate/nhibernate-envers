9b32b8ea562bcc409cc6780188fb1f9b8548c56d
oskarn <oskar.nyholm@accero.se>
2015-11-18 16:08:40 +0100

    inheriting from TestBase, fixed formatting

--- Src/NHibernate.Envers.Tests/NetSpecific/Integration/Set/Casee.cs
+++ Src/NHibernate.Envers.Tests/NetSpecific/Integration/Set/Casee.cs
@@ -3,27 +3,27 @@ using System.Collections.Generic;
 using NHibernate.Envers.Configuration.Attributes;
 using NHibernate.Envers.Tests.Integration.Inheritance.Entities;
 
-namespace NHibernate.Envers.Tests.NetSpecific.Integration.Set 
+namespace NHibernate.Envers.Tests.NetSpecific.Integration.Set
 {
-    [Audited]
-    public class Casee
-    {
+	[Audited]
+	public class Casee
+	{
 
-        public virtual int Id { get; set; }
+		public virtual int Id { get; set; }
 
-        public virtual DateTime? LastModifyDate { get; set; }
+		public virtual DateTime? LastModifyDate { get; set; }
 
-        public virtual ISet<CaseToCaseTag> CaseTags { get; set; }
+		public virtual ISet<CaseToCaseTag> CaseTags { get; set; }
 
-    }
+	}
 
-    [Audited]
-    public class CaseToCaseTag
-    {
-        
-        public virtual int Id { get; set; }
+	[Audited]
+	public class CaseToCaseTag
+	{
 
-        public virtual Casee Right { get; set; }
+		public virtual int Id { get; set; }
 
-    }
+		public virtual Casee Right { get; set; }
+
+	}
 }
