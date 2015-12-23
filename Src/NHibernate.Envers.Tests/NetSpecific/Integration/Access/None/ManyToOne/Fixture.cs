c0f741d8ca75e758fff3d1793c0dcb7bea39572c
Roger Kratz <roger.kratz@gmail.com>
2015-12-23 16:24:33 +0100

    NHE-145 Tests for many-to-one

---
+++ Src/NHibernate.Envers.Tests/NetSpecific/Integration/Access/None/ManyToOne/Fixture.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq;
+using NHibernate.Envers.Query;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace NHibernate.Envers.Tests.NetSpecific.Integration.Access.None.ManyToOne
+{
+	[Ignore("Failing tests for NHE-145")]
+	public class Fixture : TestBase
+	{
+		private int parentId;
+
+		public Fixture(string strategyType) : base(strategyType)
+		{
+		}
+
+		protected override void Initialize()
+		{
+			var parent = new Parent();
+			parent.AddChild("child1");
+			using (var tx = Session.BeginTransaction())
+			{
+				parentId = (int) Session.Save(parent);
+				tx.Commit();
+			}
+			using (var tx = Session.BeginTransaction())
+			{
+				parent.AddChild("child2");
+				tx.Commit();
+			}
+		}
+
+		[Test]
+		public void CanQueryOnAccessNoneProperty()
+		{
+			AuditReader().CreateQuery()
+				.ForHistoryOf<Child, DefaultRevisionEntity>()
+				.Add(AuditEntity.Property("ParentId").Eq(parentId))
+				.Results()
+				.Count().Should().Be.EqualTo(2);
+		}
+
+		[Test]
+		public void CanQueryOnAccessNoneManyToOne()
+		{
+			var parent = new Parent {Id = parentId};
+			AuditReader().CreateQuery()
+				.ForHistoryOf<Child, DefaultRevisionEntity>()
+				.Add(AuditEntity.Property("Parent2").Eq(parent))
+				.Results()
+				.Count().Should().Be.EqualTo(2);
+		}
+	}
+}
---
+++ Src/NHibernate.Envers.Tests/NetSpecific/Integration/Access/None/ManyToOne/Model.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using NHibernate.Envers.Configuration.Attributes;
+
+namespace NHibernate.Envers.Tests.NetSpecific.Integration.Access.None.ManyToOne
+{
+	[Audited]
+	public class Parent
+	{
+		public Parent()
+		{
+			Children = new HashSet<Child>();
+		}
+
+		public virtual int Id { get; set; } 
+		public virtual ISet<Child> Children { get; protected set; }
+
+		public virtual void AddChild(string name)
+		{
+			Children.Add(new Child
+			{
+				Parent = this,
+				Name = name
+			});
+		}
+	}
+
+	[Audited]
+	public class Child
+	{
+		public virtual int Id { get; set; }
+		public virtual string Name { get; set; }
+		public virtual Parent Parent { get; set; }
+	}
+}
