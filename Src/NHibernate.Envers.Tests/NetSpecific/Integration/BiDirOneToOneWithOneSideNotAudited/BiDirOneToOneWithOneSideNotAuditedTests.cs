4bc00c2494ead64d9ab577457bf3ef15904ad0f2
owerkop <owerkop@gmail.com>
2016-03-31 14:52:03 +0200

    NHE-150 Failing tests for bidirection one-to-one. One side marked as RelationTargetAuditMode.NotAudited.

---
+++ Src/NHibernate.Envers.Tests/NetSpecific/Integration/BiDirOneToOneWithOneSideNotAudited/BiDirOneToOneWithOneSideNotAuditedTests.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+using NHibernate.Envers.Query;
+using NUnit.Framework;
+
+namespace NHibernate.Envers.Tests.NetSpecific.Integration.BiDirOneToOneWithOneSideNotAudited
+{
+	[Ignore("Not yet fixed - NHE-150")]
+	public class BiDirOneToOneWithOneSideNotAuditedTests : TestBase
+	{
+		private Guid parentId;
+		private string childLatestStr;
+
+		public BiDirOneToOneWithOneSideNotAuditedTests(string strategyType)
+			: base(strategyType)
+		{
+		}
+
+		protected override void Initialize()
+		{
+			var parent = new Parent { Description = "parent 1 first description" };
+			parent.Child = new Child { Str = "child of first parent", Parent = parent };
+
+			//rev 0
+			using (var tx = Session.BeginTransaction())
+			{
+				Session.Save(parent);
+				Session.Save(parent.Child);
+				tx.Commit();
+			}
+
+			parentId = parent.Id;
+
+			//rev 1
+			using (var tx = Session.BeginTransaction())
+			{
+				parent.Description += "modified";
+				tx.Commit();
+			}
+
+			//rev 1 (not audited child modified)
+			using (var tx = Session.BeginTransaction())
+			{
+				parent.Child.Str += " modified";
+				tx.Commit();
+			}
+
+			//rev 2
+			using (var tx = Session.BeginTransaction())
+			{
+				parent.Description += " modified2";
+				tx.Commit();
+			}
+
+			childLatestStr = parent.Child.Str;
+		}
+
+		[Test]
+		public void QueryHistoryShouldNotThrow()
+		{
+			Assert.DoesNotThrow(() =>
+				{
+					AuditReader().CreateQuery().ForRevisionsOf<Parent>().Results().ToList();
+				});
+		}
+
+		[Test]
+		public void QueryHistoryReturnAllRevisions()
+		{
+			var parentRevisions = AuditReader().CreateQuery().ForRevisionsOf<Parent>()
+				.Add(AuditEntity.Id().Eq(parentId))
+				.Results().ToList();
+
+			Assert.AreEqual(3, parentRevisions.Count);
+		}
+
+		[Test]
+		public void ParentsHistoricalVersionsPointsToCurrentNotAuditedChild()
+		{
+			var parentRevisions = AuditReader().CreateQuery().ForRevisionsOf<Parent>()
+				.Add(AuditEntity.Id().Eq(parentId))
+				.Results().ToList();
+
+			foreach (var revision in parentRevisions)
+			{
+				Assert.AreEqual(childLatestStr, revision.Child.Str);
+			}
+		}
+	}
+}
---
+++ Src/NHibernate.Envers.Tests/NetSpecific/Integration/BiDirOneToOneWithOneSideNotAudited/Parent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NHibernate.Envers.Configuration.Attributes;
+
+namespace NHibernate.Envers.Tests.NetSpecific.Integration.BiDirOneToOneWithOneSideNotAudited
+{
+	[Audited]
+	public class Parent
+	{
+		public virtual Guid Id { get; set; }
+
+		[Audited(TargetAuditMode = RelationTargetAuditMode.NotAudited)]
+		public virtual Child Child { get; set; }
+
+		public virtual string Description { get; set; }
+	}
+}
