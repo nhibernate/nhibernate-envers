6e619aaf4b44d720bcd4bd0a4433011c9aca53e5
Roger Kratz <roger.kratz@gmail.com>
2016-12-21 12:25:29 +0100

    restrcture code in AbstractIdMapper

--- Src/NHibernate.Envers/Entities/Mapper/Id/AbstractIdMapper.cs
+++ Src/NHibernate.Envers/Entities/Mapper/Id/AbstractIdMapper.cs
@@ -8,15 +8,18 @@ namespace NHibernate.Envers.Entities.Mapper.Id
 	[Serializable]
 	public abstract class AbstractIdMapper : IIdMapper
 	{
-		private static Parameters GetParametersToUse(Parameters parameters, ICollection<QueryParameterData> paramDatas)
-		{
-			return paramDatas.Count > 1 ? parameters.AddSubParameters("and") : parameters;
-		}
+		public abstract IList<QueryParameterData> MapToQueryParametersFromId(object obj);
+		public abstract void MapToMapFromId(IDictionary<string, object> data, object obj);
+		public abstract void MapToMapFromEntity(IDictionary<string, object> data, object obj);
+		public abstract bool MapToEntityFromMap(object obj, IDictionary data);
+		public abstract object MapToIdFromEntity(object data);
+		public abstract object MapToIdFromMap(IDictionary data);
+		public abstract IIdMapper PrefixMappedProperties(string prefix);
 
 		public void AddIdsEqualToQuery(Parameters parameters, string prefix1, string prefix2) 
 		{
 			var paramDatas = MapToQueryParametersFromId(null); 
-			var parametersToUse = GetParametersToUse(parameters, paramDatas);
+			var parametersToUse = AbstractIdMapper.parametersToUse(parameters, paramDatas);
 
 			foreach (var paramData in paramDatas) 
 			{
@@ -28,7 +31,7 @@ namespace NHibernate.Envers.Entities.Mapper.Id
 		{
 			var paramDatas1 = MapToQueryParametersFromId(null);
 			var paramDatas2 = mapper2.MapToQueryParametersFromId(null); 
-			var parametersToUse = GetParametersToUse(parameters, paramDatas1);
+			var parametersToUse = AbstractIdMapper.parametersToUse(parameters, paramDatas1);
 
 			for (var i = 0; i < paramDatas1.Count; i++)
 			{
@@ -41,7 +44,7 @@ namespace NHibernate.Envers.Entities.Mapper.Id
 		public void AddIdEqualsToQuery(Parameters parameters, object id, string prefix, bool equals) 
 		{
 			var paramDatas = MapToQueryParametersFromId(id);
-			var parametersToUse = GetParametersToUse(parameters, paramDatas);
+			var parametersToUse = AbstractIdMapper.parametersToUse(parameters, paramDatas);
 
 			foreach (var paramData in paramDatas) 
 			{
@@ -56,6 +59,23 @@ namespace NHibernate.Envers.Entities.Mapper.Id
 			}
 		}
 
+		public void AddNamedIdEqualsToQuery(Parameters parameters, string prefix, bool equals) 
+		{
+			var paramDatas = MapToQueryParametersFromId(null);
+			var parametersToUse = AbstractIdMapper.parametersToUse(parameters, paramDatas);
+
+			foreach (var paramData in paramDatas) 
+			{
+				parametersToUse.AddWhereWithNamedParam(paramData.GetProperty(prefix), equals ? "=" : "<>", paramData.QueryParameterName);
+			}
+		}
+
+
+		private static Parameters parametersToUse(Parameters parameters, ICollection<QueryParameterData> paramDatas)
+		{
+			return paramDatas.Count > 1 ? parameters.AddSubParameters("and") : parameters;
+		}
+
 		private static void handleNullValue(Parameters parameters, string propertyName, bool equals)
 		{
 			if (equals)
@@ -67,24 +87,5 @@ namespace NHibernate.Envers.Entities.Mapper.Id
 				parameters.AddNotNullRestriction(propertyName, equals);
 			}
 		}
-
-		public void AddNamedIdEqualsToQuery(Parameters parameters, string prefix, bool equals) 
-		{
-			var paramDatas = MapToQueryParametersFromId(null);
-			var parametersToUse = GetParametersToUse(parameters, paramDatas);
-
-			foreach (var paramData in paramDatas) 
-			{
-				parametersToUse.AddWhereWithNamedParam(paramData.GetProperty(prefix), equals ? "=" : "<>", paramData.QueryParameterName);
-			}
-		}
-
-		public abstract IList<QueryParameterData> MapToQueryParametersFromId(object obj);
-		public abstract void MapToMapFromId(IDictionary<string, object> data, object obj);
-		public abstract void MapToMapFromEntity(IDictionary<string, object> data, object obj);
-		public abstract bool MapToEntityFromMap(object obj, IDictionary data);
-		public abstract object MapToIdFromEntity(object data);
-		public abstract object MapToIdFromMap(IDictionary data);
-		public abstract IIdMapper PrefixMappedProperties(string prefix);
 	}
 }
