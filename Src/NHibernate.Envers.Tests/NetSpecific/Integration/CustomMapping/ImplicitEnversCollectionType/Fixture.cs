e2e8c662ba23f622949d37ea60c2f0774139d2a1
Roger Kratz <roger.kratz@gmail.com>
2017-01-13 16:14:31 +0100

    NHE-157 fixed. Don't force user to create ICustomCollectionMapperFactory if using custom NH user collection type

---
+++ Src/NHibernate.Envers.Tests/NetSpecific/Integration/CustomMapping/ImplicitEnversCollectionType/Fixture.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace NHibernate.Envers.Tests.NetSpecific.Integration.CustomMapping.ImplicitEnversCollectionType
+{
+	public class Fixture : TestBase
+	{
+		private int parentId;
+
+		public Fixture(string strategyType) : base(strategyType)
+		{
+		}
+
+		protected override void Initialize()
+		{
+			var parent = new Parent {Children = new List<Child> {new Child()}};
+
+			using (var tx = Session.BeginTransaction())
+			{
+				parentId = (int) Session.Save(parent);
+				tx.Commit();
+			}
+
+			using (var tx = Session.BeginTransaction())
+			{
+				parent.Children.Add(new Child());
+				tx.Commit();
+			}
+		}
+
+		[Test]
+		public void VerifyNhCoreMapping()
+		{
+			var entity = Session.Get<Parent>(parentId);
+			entity.Children.Count
+				.Should().Be.EqualTo(2);
+		}
+
+		[Test]
+		public void VerifyRevision1()
+		{
+			var entity = AuditReader().Find<Parent>(parentId, 1);
+			entity.Children.Count
+				.Should().Be.EqualTo(1);
+		}
+
+		[Test]
+		public void VerifyRevision2()
+		{
+			var entity = AuditReader().Find<Parent>(parentId, 2);
+			entity.Children.Count
+				.Should().Be.EqualTo(2);
+		}
+	}
+}
---
+++ Src/NHibernate.Envers.Tests/NetSpecific/Integration/CustomMapping/ImplicitEnversCollectionType/Model.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using NHibernate.Envers.Configuration.Attributes;
+
+namespace NHibernate.Envers.Tests.NetSpecific.Integration.CustomMapping.ImplicitEnversCollectionType
+{
+	[Audited]
+	public class Parent
+	{
+		public virtual int Id { get; set; }
+		public virtual IList<Child> Children { get; set; }
+	}
+
+	[Audited]
+	public class Child
+	{
+		public virtual int Id { get; set; }
+	}
+}
