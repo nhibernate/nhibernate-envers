98022e68cc508f157c8da8dbaaa6d9a3a1652696
Patryk Marek <patryk.piotr.marek@gmail.com>
2017-03-03 10:02:23 +0100

    NHE-139 ignored failing test (child collection in subclass with inverse=false)

---
+++ Src/NHibernate.Envers.Tests/NetSpecific/Integration/MultiLevelInheritance/InheritedParent.cs
@@ -0,0 +1,10 @@
+﻿using NHibernate.Envers.Configuration.Attributes;
+
+namespace NHibernate.Envers.Tests.NetSpecific.Integration.MultiLevelInheritance
+{
+	[Audited]
+	public class InheritedParent : Parent
+	{
+		public virtual string InheritedProperty { get; set; }
+	}
+}
