be3840165c991a350a60a07ede90cbfe817345af
Patryk Marek <patryk.piotr.marek@gmail.com>
2017-03-06 11:17:37 +0100

    NHE-158 ignored failing test (two relations with same column)

---
+++ Src/NHibernate.Envers.Tests/NetSpecific/UnitTests/Bidirectional/ModelShared.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace NHibernate.Envers.Tests.NetSpecific.UnitTests.Bidirectional
+{
+	public class ModelShared
+	{
+		public virtual int Id { get; set; }
+
+		public virtual ISet<ModelConfigurationShared> ModelConfigurations { get; set; }
+	}
+}
