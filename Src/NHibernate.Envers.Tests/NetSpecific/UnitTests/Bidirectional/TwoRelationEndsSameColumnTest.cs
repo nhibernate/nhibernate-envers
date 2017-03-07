fea6871636ac2810959de2aa7420a98eec67070b
Patryk Marek <patryk.piotr.marek@gmail.com>
2017-03-07 08:45:52 +0100

    NHE-158 more asserts

--- Src/NHibernate.Envers.Tests/NetSpecific/UnitTests/Bidirectional/TwoRelationEndsSameColumnTest.cs
+++ Src/NHibernate.Envers.Tests/NetSpecific/UnitTests/Bidirectional/TwoRelationEndsSameColumnTest.cs
@@ -1,4 +1,6 @@
-﻿using NHibernate.Cfg;
+﻿using System.Collections.Generic;
+using System.Linq;
+using NHibernate.Cfg;
 using NUnit.Framework;
 
 namespace NHibernate.Envers.Tests.NetSpecific.UnitTests.Bidirectional
@@ -6,13 +8,66 @@ namespace NHibernate.Envers.Tests.NetSpecific.UnitTests.Bidirectional
 	public class TwoRelationEndsSameColumnTest
 	{
 		[Test]
-		public void ShouldNotThrowMappingException()
+		public void ShouldIntegrateWithEnversNotThrowMappingException()
+		{
+			Assert.DoesNotThrow(() => getConfiguration());		
+		}
+
+		[Test]
+		public void ShouldAuditModelProperly()
+		{
+			var cfg = getConfiguration();
+			var session = getSession(cfg);
+
+			const int modelId = 112;
+			const int modelConfigurationId = 2987;
+
+			var model = new Model { Id = modelId, ModelConfigurations = new HashSet<ModelConfigurationShared>() };
+			var modelShared = new ModelShared { Id = modelId, ModelConfigurations = new HashSet<ModelConfigurationShared>() };
+			var modelConfigurationShared = new ModelConfigurationShared { Id = modelConfigurationId };
+
+			using (var tx = session.BeginTransaction())
+			{
+				session.Save(model);
+				session.Save(modelShared);
+				session.Save(modelConfigurationShared);
+
+				model.ModelConfigurations.Add(modelConfigurationShared);
+				modelShared.ModelConfigurations.Add(modelConfigurationShared);
+				modelConfigurationShared.Model = modelShared;
+
+				session.Save(model);
+				session.Save(modelShared);
+				session.Save(modelConfigurationShared);
+
+				tx.Commit();
+			}
+
+			var modelFromAudit = session.Auditer().CreateQuery()
+										.ForRevisionsOf<Model>().Single();
+
+			Assert.AreEqual(model.Id, modelFromAudit.Id);
+			Assert.AreEqual(modelConfigurationShared.Id, modelFromAudit.ModelConfigurations.Single().Id);
+			Assert.AreEqual(modelShared.Id, modelFromAudit.ModelConfigurations.Single().Model.Id);
+		}
+
+		private Cfg.Configuration getConfiguration()
 		{
 			var cfg = new Cfg.Configuration();
+
 			cfg.Configure();
 			cfg.AddResource("NHibernate.Envers.Tests.NetSpecific.UnitTests.Bidirectional.Mapping.hbm.xml", GetType().Assembly);
+			cfg.IntegrateWithEnvers();
 
-			Assert.DoesNotThrow(() => cfg.IntegrateWithEnvers());
+			return cfg;
+		}
+
+		private static ISession getSession(Cfg.Configuration cfg)
+		{
+			var sessionFactory = cfg.BuildSessionFactory();
+			var session = sessionFactory.OpenSession();
+			session.FlushMode = FlushMode.Auto;
+			return session;
 		}
 	}
 }
