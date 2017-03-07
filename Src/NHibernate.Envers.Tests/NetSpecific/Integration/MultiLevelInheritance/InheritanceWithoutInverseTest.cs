25fde999ab298e9dee0aab6d647e2e84819d89a1
Patryk Marek <patryk.piotr.marek@gmail.com>
2017-03-07 09:56:53 +0100

    NHE-139 more asserts

--- Src/NHibernate.Envers.Tests/NetSpecific/Integration/MultiLevelInheritance/InheritanceWithoutInverseTest.cs
+++ Src/NHibernate.Envers.Tests/NetSpecific/Integration/MultiLevelInheritance/InheritanceWithoutInverseTest.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 
 namespace NHibernate.Envers.Tests.NetSpecific.Integration.MultiLevelInheritance
@@ -9,20 +10,39 @@ namespace NHibernate.Envers.Tests.NetSpecific.Integration.MultiLevelInheritance
 		{
 		}
 
+		private InheritedParent parent;
+
 		protected override void Initialize()
 		{
+			parent = new InheritedParent { Childs = new HashSet<Child>() };
+			parent.Childs.Add(new Child { Parent = parent });
 		}
 
 		[Test]
-		public void Test()
+		public void ShouldNotThrowExceptionOnCreatingAudit()
 		{
-			var parent = new InheritedParent {Childs = new HashSet<Child>()};
-			parent.Childs.Add(new Child { Parent = parent });
+			Assert.DoesNotThrow(saveParent);
+		}
 
+		[Test]
+		public void ShouldAuditParentAndChildProperly()
+		{
+			saveParent();
+			var auditedInheritedParent = AuditReader().CreateQuery().ForRevisionsOf<InheritedParent>().Single();
+			var auditedParent = AuditReader().CreateQuery().ForRevisionsOf<Parent>().Single();
+			var auditedChild = AuditReader().CreateQuery().ForRevisionsOf<Child>().Single();
+
+			Assert.AreEqual(parent.Id, auditedInheritedParent.Id);
+			Assert.AreEqual(parent.Id, auditedParent.Id);
+			Assert.AreEqual(parent.Childs.Single().Id, auditedChild.Id);
+		}
+
+		private void saveParent()
+		{
 			using (var tx = Session.BeginTransaction())
 			{
 				Session.Save(parent);
-				Assert.DoesNotThrow(() => tx.Commit());
+				tx.Commit();
 			}
 		}
 	}
