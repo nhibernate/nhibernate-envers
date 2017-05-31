a8ea2592c2fbaeb82b28ce5b86596fe11b30188f
Roger Kratz <roger.kratz@gmail.com>
2017-05-31 10:16:39 +0200

    NHE-159 Fixed

---
+++ Src/NHibernate.Envers.Tests/Integration/ModifiedFlags/HasChangedInsertUpdateSameTransactionTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using NHibernate.Envers.Tests.Integration.Basic;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace NHibernate.Envers.Tests.Integration.ModifiedFlags
+{
+	public class HasChangedInsertUpdateSameTransactionTest : AbstractModifiedFlagsEntityTest
+	{
+		public HasChangedInsertUpdateSameTransactionTest(string strategyType) : base(strategyType)
+		{
+		}
+
+		protected override void Initialize()
+		{
+			using (var tx = Session.BeginTransaction())
+			{
+				var entity = new BasicTestEntity1 {Long1 = 1, Str1 = "str1"};
+				Session.Save(entity);
+				entity.Str1 = "str2";
+				Session.Merge(entity);
+				tx.Commit();
+			}
+		}
+
+		[Test]
+		public void VerifyPropertyChangedInsertUpdateSameTransaction()
+		{
+			QueryForPropertyHasChanged(typeof(BasicTestEntity1), 1, "Long1").Count
+				.Should().Be.EqualTo(1);
+		}
+
+		protected override IEnumerable<string> Mappings => new[] { "Integration.Basic.Mapping.hbm.xml" };
+	}
+}
