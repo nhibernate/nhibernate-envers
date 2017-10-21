29508803912f2106b54b8df1103e742c8ef075f8
Rico Pfennig <pfennig@softwareparadies.de>
2017-10-21 21:30:12 +0200

    Added test for issue NHE-161: https://nhibernate.jira.com/browse/NHE-161

---
+++ Src/NHibernate.Envers.Tests/NetSpecific/Integration/RevisionOnCollectionChange/Domain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using NHibernate.Envers.Configuration.Attributes;
+
+namespace NHibernate.Envers.Tests.NetSpecific.Integration.RevisionOnCollectionChange
+{
+	[Audited]
+	public class Person
+	{
+		public Person()
+		{
+			Cars = new List<Car>();
+		}
+		public virtual Guid Id { get; set; }
+
+		public virtual IList<Car> Cars { get; protected set; }
+	}
+
+	[Audited]
+	public class Car
+	{
+		public virtual Guid Id { get; set; }
+
+		public virtual Person Owner { get; set; }
+	}
+}
