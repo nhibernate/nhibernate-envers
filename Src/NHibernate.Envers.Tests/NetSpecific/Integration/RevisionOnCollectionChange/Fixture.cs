94e14830bf82f37efad1c6c255e878fe8f2de275
Rico Pfennig <pfennig@softwareparadies.de>
2017-10-21 21:30:12 +0200

    Added test for issue NHE-161: https://nhibernate.jira.com/browse/NHE-161

---
+++ Src/NHibernate.Envers.Tests/NetSpecific/Integration/RevisionOnCollectionChange/Fixture.cs
@@ -0,0 +1,49 @@
+﻿using NHibernate.Cfg;
+using NHibernate.Envers.Configuration;
+using NUnit.Framework;
+
+namespace NHibernate.Envers.Tests.NetSpecific.Integration.RevisionOnCollectionChange
+{
+	[TestFixture]
+	public class Fixture : TestBase
+	{
+		private object _newOwnerId;
+		private object _carId;
+
+		public Fixture(string strategyType) : base(strategyType)
+		{
+		}
+
+		protected override void AddToConfiguration(Cfg.Configuration configuration)
+		{
+			configuration.SetEnversProperty(ConfigurationKey.GlobalWithModifiedFlag, true);
+			configuration.SetEnversProperty(ConfigurationKey.RevisionOnCollectionChange, true);
+			base.AddToConfiguration(configuration);
+		}
+
+		protected override void Initialize()
+		{
+			using (var tx = Session.BeginTransaction())
+			{
+				var oldOwner = new Person();
+				Session.Save(oldOwner);
+				var newOwner = new Person();
+				_newOwnerId = Session.Save(newOwner);
+				var car = new Car { Owner = oldOwner };
+				_carId = Session.Save(car);
+				tx.Commit();
+			}
+		}
+
+		[Test]
+		public void ChangeParentReferenceShouldNotThwrowException()
+		{
+			using (var tx = Session.BeginTransaction())
+			{
+				var car = Session.Get<Car>(_carId);
+				car.Owner = Session.Get<Person>(_newOwnerId);
+				tx.Commit();
+			}
+		}
+	}
+}
