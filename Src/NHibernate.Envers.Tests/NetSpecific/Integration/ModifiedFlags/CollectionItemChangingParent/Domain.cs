190b427a2468b5e97fde147a2ddec8ef9b6f81d9
Roger Kratz <roger.kratz@gmail.com>
2017-10-22 15:23:28 +0200

    NHE-161 Moving tests to sub dir of ModifiedFlags

--- Src/NHibernate.Envers.Tests/NetSpecific/Integration/ModifiedFlags/CollectionItemChangingParent/Domain.cs
+++ Src/NHibernate.Envers.Tests/NetSpecific/Integration/ModifiedFlags/CollectionItemChangingParent/Domain.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using NHibernate.Envers.Configuration.Attributes;
 
-namespace NHibernate.Envers.Tests.NetSpecific.Integration.RevisionOnCollectionChange
+namespace NHibernate.Envers.Tests.NetSpecific.Integration.ModifiedFlags.CollectionItemChangingParent
 {
 	[Audited]
 	public class Person
