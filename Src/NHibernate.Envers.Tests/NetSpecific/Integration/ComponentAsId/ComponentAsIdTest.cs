ab350dc63a5a05086720f97cf7df52dd2d82ef0c
ogorodnikovInfopulse <Vladyslav.Ohorodnikov@infopulse.com>
2019-05-07 11:01:18 +0300

    Failing test for #11

---
+++ Src/NHibernate.Envers.Tests/NetSpecific/Integration/ComponentAsId/ComponentAsIdTest.cs
@@ -0,0 +1,68 @@
+﻿using NUnit.Framework;
+
+namespace NHibernate.Envers.Tests.NetSpecific.Integration.ComponentAsId
+{
+	public class ComponentAsIdTest : TestBase
+	{
+		public ComponentAsIdTest(AuditStrategyForTest strategyType) : base(strategyType)
+		{
+		}
+		
+		[Test]
+		public void ComponentAsIdTestMethod()
+		{
+			Assert.DoesNotThrow(() =>
+			{
+				var ent1 = new Entity1
+				{
+					Id = 1
+				};
+
+				Save(ent1);
+
+				var ent2 = new Entity2()
+				{
+					Id = 1
+				};
+
+				Save(ent2);
+
+				var udf = new SomeEntUDF
+				{
+					Id = new ComponentAsId
+					{
+						Key1 = ent1,
+						Key2 = ent2
+					}
+				};
+
+				Save(udf);
+
+				Del(udf);
+				Del(ent1);
+			});
+		}
+
+		void Save(object o)
+		{
+			using (var tran = Session.BeginTransaction())
+			{
+				Session.Save(o);
+				tran.Commit();
+			}
+		}
+
+		void Del(object o)
+		{
+			using (var tran = Session.BeginTransaction())
+			{
+				Session.Delete(o);
+				tran.Commit();
+			}
+		}
+
+		protected override void Initialize()
+		{
+		}
+	}
+}
---
+++ Src/NHibernate.Envers.Tests/NetSpecific/Integration/ComponentAsId/Entity.cs
@@ -0,0 +1,49 @@
+﻿using NHibernate.Envers.Configuration.Attributes;
+
+namespace NHibernate.Envers.Tests.NetSpecific.Integration.ComponentAsId
+{
+	public class ComponentAsId
+	{
+		public Entity2 Key2 { get; set; }
+		public Entity1 Key1 { get; set; }
+
+		public override bool Equals(object obj)
+		{
+			if (obj == null)
+				return false;
+
+			if (!(obj is ComponentAsId))
+				return false;
+
+			if (!ReferenceEquals(this, obj))
+				return false;
+
+			var other = obj as ComponentAsId;
+
+			return Key1.Id == other.Key1.Id && Key2.Id == other.Key2.Id;
+		}
+
+		public override int GetHashCode()
+		{
+			return 1;
+		}
+	}
+
+	[Audited]
+	public class Entity1
+	{
+		public virtual int Id { get; set; }
+	}
+
+	[Audited]
+	public class Entity2
+	{
+		public virtual int Id { get; set; }
+	}
+
+	[Audited]
+	public class SomeEntUDF
+	{
+		public virtual ComponentAsId Id { get; set; }
+	}
+}
