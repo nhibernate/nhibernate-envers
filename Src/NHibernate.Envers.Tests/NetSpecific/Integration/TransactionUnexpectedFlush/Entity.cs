e95454c7deb309edd5dfb15ef15ea147502432b4
Anna Khaperskaia <AKhaperskaya@luxoft.com>
2019-07-26 18:05:38 +0300

    Add test to illustrate bug: calling 'GetCurrentRevision' in the middle of uncommited transaction leads to unexpected flush

---
+++ Src/NHibernate.Envers.Tests/NetSpecific/Integration/TransactionUnexpectedFlush/Entity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using NHibernate.Envers.Configuration.Attributes;
+
+namespace NHibernate.Envers.Tests.NetSpecific.Integration.TransactionUnexpectedFlush
+{
+	[Audited]
+	public class Entity
+	{
+		public virtual Guid Id { get; set; }
+
+		public virtual string Name { get; set; }
+	}
+}
