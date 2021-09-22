08aaf8608b01d74f8529e88583267e98f715329d
Roger Kratz <roger.kratz@gmail.com>
2021-09-22 11:57:23 +0200

    workflow running against 5 different dbs

---
+++ Src/NHibernate.Envers.Tests/Tools/ConfigurationExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+namespace NHibernate.Envers.Tests.Tools
+{
+	public static class ConfigurationExtensions
+	{
+		public static Cfg.Configuration OverrideSettingsFromEnvironmentVariables(this Cfg.Configuration configuration)
+		{
+			foreach (var nhProperty in configuration.Properties.ToArray())
+			{
+				var envVar = System.Environment.GetEnvironmentVariable(nhProperty.Key);
+				if (envVar != null)
+				{
+					configuration.SetProperty(nhProperty.Key, envVar);
+				}
+			}
+			
+			return configuration;
+		}
+	}
+}
